f215f5bc134c6d0011fa0cb6f58ed8bc484ec491
Julius Hrivnac <julius.hrivnac@cern.ch>
2019-07-26 14:25:33 +0200

    system cleaning

--- ant/setup.sh
+++ ant/setup.sh
@@ -1,2 +1,11 @@
 source build-global.properties
 source build-local.properties
+
+if [[ ! "x" = "x${ant_home}" ]]; then
+  export ANT_HOME="${ant_home}"
+  export PATH="${ANT_HOME}/bin:${PATH}"
+  fi
+if [[ ! "x" = "x${java_home}" ]]; then
+  export JAVA_HOME="${java_home}"
+  export PATH="${JAVA_HOME}/bin:${PATH}"
+  fi
