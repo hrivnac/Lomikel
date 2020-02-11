f91741cc4f4ec5e480b2dcea15fa573356bfe38f
Julius Hrivnac <julius.hrivnac@cern.ch>
2020-02-11 11:29:15 +0100

    ignoring

--- src/sh/startJanusGraph.sh
+++ src/sh/startJanusGraph.sh
@@ -1,2 +1,3 @@
 echo "Logging to ../run/janusgraph.log"
+${janusgraph_dir}/elasticsearch/bin/elasticsearch -d
 ${janusgraph_dir}/bin/gremlin-server.sh ${janusgraph_dir}/conf/gremlin-server/jhtools.yaml > ../run/janusgrasp.log 2>&1 &
