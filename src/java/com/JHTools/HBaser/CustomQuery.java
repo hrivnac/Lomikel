5b6748c029eb29afc3f39ed44e683892b47aaec9
Julius Hrivnac <julius.hrivnac@cern.ch>
2020-05-28 15:23:55 +0200

    special HBase searches

---
+++ src/java/com/JHTools/HBaser/CustomQuery.java
@@ -0,0 +1,17 @@
+package com.JHTools.HBaser;
+
+// Log4J
+import org.apache.log4j.Logger;
+
+/** <code>CustomQuery</code> defines a special HBase query. 
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public class CustomQuery {
+
+  /** Logging . */
+  private static Logger log = Logger.getLogger(CustomQuery.class);
+
+  }
