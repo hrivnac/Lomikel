de9f52dc3ec4e6c8cbab5dc97286e0353be8223a
Julius Hrivnac <julius.hrivnac@cern.ch>
2020-06-05 15:47:52 +0200

    better CLI, faster HBase

---
+++ src/java/com/Lomikel/Apps/Console.java
@@ -0,0 +1,68 @@
+package com.Lomikel.Apps;
+
+// AWT
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Dimension;
+
+// Swing
+import javax.swing.JScrollPane;
+import javax.swing.BorderFactory;
+
+// Bean Shell
+import bsh.util.JConsole;
+import bsh.Interpreter;
+
+// Log4J
+import org.apache.log4j.Logger;
+
+/** Bean Shell {@link JConsole}.
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public final class Console extends JConsole {
+
+  /** Create. TBD */
+  public Console() {
+    _this = this;
+    setFont(new Font("Helvetica", Font.PLAIN, 15));
+    setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+    setBorder(BorderFactory.createEtchedBorder());
+    setSize(                     Integer.MAX_VALUE, 800);
+    setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
+    setMinimumSize(new Dimension(600,               800));
+    setWaitFeedback(true);
+    }    
+    
+  /** Add text to {@link JConsole}.
+    * Write to <em>stdout</em> if {@link JConsole} not yet initialised.
+    * @param text The text to be added to {@link JConsole}. */
+  public static void setText(String text) {
+    if (_this == null) {
+      //System.out.println(text);
+      }
+    else {
+      _this.print(text + "\n", new java.awt.Font("Helvetica", java.awt.Font.PLAIN, 20), java.awt.Color.blue);
+      }
+    }
+    
+  /** Add error text to {@link JConsole}.
+    * Write to <em>stderr</em> if {@link JConsole} not yet initialised.
+    * @param text The error text to be added to  {@link JConsole}. */
+  public static void setError(String text) {
+    if (_this == null) {
+      //System.err.println(text);
+      }
+    else {
+      _this.print(text + "\n", new java.awt.Font("Helvetica", java.awt.Font.PLAIN, 20), java.awt.Color.red);
+      }
+    }
+  
+  private static Console _this;
+    
+  /** Logging . */
+  private static Logger log = Logger.getLogger(Console.class);
+
+  }
---
+++ src/java/com/Lomikel/Utils/Init.java
@@ -0,0 +1,40 @@
+package com.Lomikel.Utils;
+
+// Log4J
+import org.apache.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Level;
+import org.apache.log4j.PropertyConfigurator;
+
+// Java
+import java.util.Enumeration;
+
+/** <code>Init</code> provides common initialisation.
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public class Init {
+
+  /** Setup Logging system. */
+  public static void init() {
+    init(false);
+    }
+
+  /** Setup Logging system.
+    * @param quiet If no outupt is required. */
+  public static void init(boolean quiet) {
+    try {
+      PropertyConfigurator.configure(Init.class.getClassLoader().getResource("com/Lomikel/Utils/log4j.properties"));
+      NotifierURL.notify("", "Lomikel", Info.release());
+      }
+    catch (Exception e) {
+      System.err.println(e);
+      }
+    }
+    
+  /** Logging . */
+  private static Logger log = Logger.getLogger(Init.class);
+
+  }
