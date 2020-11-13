f2a8668ce54b9dd4c31635cd4ab09a1c52ff45c4
Julius Hrivnac <julius.hrivnac@cern.ch>
2020-11-13 15:52:55 +0100

    better scatterplot

---
+++ src/java/com/Lomikel/WebService/DataRepository.java
@@ -0,0 +1,52 @@
+package com.Lomikel.WebService;
+
+// Java
+import java.util.Map;
+import java.util.HashMap;
+
+// Log4J
+import org.apache.log4j.Logger;
+
+/** <code>DataRepository</code> communicates {@link String} data between applets..
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public class DataRepository {
+    
+  /** Add data.
+    * @param name    The name of the data.
+    * @param content The data content. */
+  public void add(String name,
+                  String content) {
+    _data.put(name, content);
+    }
+    
+  /** Give data.
+    * @param name The name of the data.
+    * @return     The data content. */
+  public String get(String name) {
+    return _data.get(name);
+    }
+  
+  /** Give data and remove them.
+    * @param name The name of the data.
+    * @return     The data content. */
+  public String getAndRemove(String name) {
+    String content = _data.get(name);
+    _data.remove(name);
+    return content;
+    }
+
+  /** Clear the repository. */
+  public void clear() {
+    _data.clear();
+    }
+    
+  private Map<String, String> _data = new  HashMap<>();
+  
+  /** Logging . */
+  private static Logger log = Logger.getLogger(DataRepository.class);
+
+  }
---
+++ src/ws/d3/scatterplot.js
@@ -0,0 +1,46 @@
+function showScatterPlot(dataS, name, xS, yS) {
+  var w = 450;
+  var h = 400;
+  var margin = {top:10, right:40, bottom:30, left:30},
+               width =  w - margin.left - margin.right,
+               height = h - margin.top  - margin.bottom;
+  var svg = d3.select("#scatter_area")
+              .append("svg")
+              .attr("width",  width  + margin.left + margin.right)
+              .attr("height", height + margin.top  + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var data = JSON.parse(dataS.replace(/'/g, '"'));
+  var x = d3.scaleLinear()
+            .domain(d3.extent(data, d => d.x)).nice()
+            .range([0, width]);      
+  svg.append('g')
+     .attr("transform", "translate(0," + height + ")")
+     .call(d3.axisBottom(x))
+     .call(g => g.append("text")
+                 .attr("x", w - margin.right)
+                 .attr("y", -4)
+                 .attr("fill", "#000")
+                 .attr("font-weight", "bold")
+                 .attr("text-anchor", "end")
+                 .text(xS));
+  var y = d3.scaleLinear()         
+            .domain(d3.extent(data, d => d.y)).nice()
+            .range([height, 0]);   
+  svg.append('g')
+     .call(d3.axisLeft(y))
+     .call(g => g.append("text")
+                 .attr("x", 4)
+                 .attr("y", 4)
+                 .attr("fill", "#000")
+                 .attr("font-weight", "bold")
+                 .attr("text-anchor", "start")
+                 .text(yS));
+  svg.selectAll("whatever")
+     .data(data)
+     .enter()
+     .append("circle")
+     .attr("cx", function(d){return x(d.x)})
+     .attr("cy", function(d){return y(d.y)})
+     .attr("r",  function(d){return   d.z})
+  }
