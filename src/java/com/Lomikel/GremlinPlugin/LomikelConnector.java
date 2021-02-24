7b9c61bc56f8f26c9217ed16366d409e6d5a92b1
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-02-24 15:36:37 +0100

    alert outsourced to FinkBrowser

--- src/java/com/Lomikel/GremlinPlugin/LomikelConnector.java
+++ src/java/com/Lomikel/GremlinPlugin/LomikelConnector.java
@@ -2,7 +2,6 @@ package com.Lomikel.GremlinPlugin;
 
 import com.Lomikel.Januser.Wertex;
 import com.Lomikel.Januser.Hertex;
-import com.Lomikel.Januser.Alert;
 import com.Lomikel.Januser.Dataset;
 import com.Lomikel.HBaser.HBaseClient;
 import com.Lomikel.Phoenixer.PhoenixProxyClient;
@@ -33,7 +32,6 @@ public class LomikelConnector extends AbstractGremlinPlugin {
           imports(new Class[]{PhoenixProxyClient.class,
                               Wertex.class,
                               Hertex.class,
-                              Alert.class,
                               Dataset.class,
                               HBaseClient.class}),
           new LomikelConsoleCustomizer());
