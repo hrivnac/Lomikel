090ec4b0d0bb443a627beb15986a60124d5ab7ce
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-02-25 19:20:21 +0100

    consolidation

---
+++ src/sys/janusgraph/scripts/setup-lomikel.groovy
@@ -0,0 +1,10 @@
+def globals = [:]
+
+globals << [hook : [
+  onStartUp: { ctx -> ctx.logger.info("Executed once at startup of Gremlin Server.")},
+  onShutDown: { ctx -> ctx.logger.info("Executed once at shutdown of Gremlin Server.")}
+  ] as LifeCycleHook]
+  
+globals << [graph : JanusGraphFactory.build().set("storage.backend", "hbase").set("storage.hostname", "@STORAGE.HOSTNAME@").set("storage.hbase.table", "@STORAGE.JANUS.TABLE@").open()]
+globals << [g : graph.traversal()]
+
