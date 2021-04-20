39b47e8486e7e24b86b1eb7d92752821cdcb23d9
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-04-20 11:09:05 +0200

    CERN

--- src/java/com/Lomikel/Phoenixer/PhoenixClient.java
+++ src/java/com/Lomikel/Phoenixer/PhoenixClient.java
@@ -200,6 +200,7 @@ public class PhoenixClient extends Client<String, PhoenixSchema> {
       for (String r : line.split("#")) {
         if (!r.trim().equals("")) {
           keyvalue = r.split("=");
+          log.info(keyvalue);
           result.put(keyvalue[0], keyvalue[1]);
           }
         }
