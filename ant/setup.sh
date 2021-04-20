3735490065fb2c6fc5ae34236b3a25e6106da04e
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-04-20 09:39:00 +0200

    CERN

--- ant/setup.sh
+++ ant/setup.sh
@@ -12,6 +12,11 @@ if [[ ! "x" = "x${java_home}" ]]; then
 if [[ ! "x" = "x${hbase_classpath}" ]]; then
   export CLASSPATH="${hbase_classpath}"
   fi
+if [[ `uname -a  |awk '{print $2}' | awk -F. '{print $2}'` = "cern" ]]; then
+  export GREMLIN_CLASSPATH="/opt/hadoop/conf/etc/analytix/hadoop.analytix:/opt/hadoop/conf/etc/analytix/hbase.analytix"
+else
+  export GREMLIN_CLASSPATH=""
+  fi
   
 export janusgraph_dir
 export hbase_dir
@@ -21,10 +26,10 @@ export hbase_table
 
 export home=`pwd`/..
 
-alias gremlin_Local='CLASSPATH=""   ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_Local.gremlin"'
-alias gremlin_IJCLab='CLASSPATH=""  ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_IJCLab.gremlin"'
-alias gremlin_CERN='CLASSPATH=""    ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_CERN.gremlin"'
-alias gremlin_console='CLASSPATH="" ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_console.gremlin ${janusgraph_dir}/conf/gremlin-server/Local.properties ${home}"'
+alias gremlin_Local='CLASSPATH="${GREMLIN_CLASSPATH}"   ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_Local.gremlin"'
+alias gremlin_IJCLab='CLASSPATH="${GREMLIN_CLASSPATH}"  ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_IJCLab.gremlin"'
+alias gremlin_CERN='CLASSPATH="${GREMLIN_CLASSPATH}"    ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_CERN.gremlin"'
+alias gremlin_console='CLASSPATH="${GREMLIN_CLASSPATH}" ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_console.gremlin ${janusgraph_dir}/conf/gremlin-server/Local.properties ${home}"'
 
 echo "commands: gremlin_console, gremlin_Local, gremlin_IJCLab, gremlin_CERN"
 
