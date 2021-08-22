7101f4724f66ae63f84a404bb5db4e32d73a31de
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-08-22 10:03:01 +0200

    new  hbase @ijclab

--- src/sys/janusgraph/scripts/setup-lomikel.groovy
+++ src/sys/janusgraph/scripts/setup-lomikel.groovy
@@ -5,7 +5,7 @@ globals << [hook : [
   onShutDown: { ctx -> ctx.logger.info("Executed once at shutdown of Gremlin Server.")}
   ] as LifeCycleHook]
   
-globals << [graph : JanusGraphFactory.build().set("storage.backend", "hbase").set("storage.hostname", "@STORAGE.HOSTNAME@").set("storage.hbase.table", "@STORAGE.JANUS.TABLE@").open()]
+globals << [graph : JanusGraphFactory.build().set("storage.backend", "hbase").set("storage.hostname", "@STORAGE.HOSTNAME@").set("storage.port", "@STORAGE.PORT@").set("storage.hbase.table", "@STORAGE.JANUS.TABLE@").open()]
 globals << [g : graph.traversal()]
 
 def hi_lomikel() {
