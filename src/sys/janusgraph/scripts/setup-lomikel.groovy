65237a8fdaa73ed6b13227f68766a5c2031ec2b7
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-11-05 12:26:08 +0100

    ...

--- src/sys/janusgraph/scripts/setup-lomikel.groovy
+++ src/sys/janusgraph/scripts/setup-lomikel.groovy
@@ -16,9 +16,16 @@ def get_or_create(lbl, name, value) {
   return g.V().has('lbl', lbl).
                has(name, value).
                fold().
-               coalesce(unfold(),
-                        addV(lbl).
-                        property('lbl', lbl).
-                        property(name, value));
+               coalesce(unfold(), addV(lbl).
+                                  property('lbl', lbl).
+                                  property(name, value));
   }
              
+def get_or_create_edge(g, lbl1, name1, value1, lbl2, name2, value2, edge) {
+  return g.V().has('lbl', lbl1).
+               has(name1, value1).
+               as('v').
+           V().has('lbl', lbl2).
+               has(name2, value2).
+           coalesce(__.inE(edge).where(outV().as('v')), addE(edge).from('v'));
+    }
