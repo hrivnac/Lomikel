48ae875b6345a3b661ea8dfb94e60a8a51ab52a5
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-11-18 12:07:44 +0100

    ...

--- src/java/com/Lomikel/Januser/Sertex.java
+++ src/java/com/Lomikel/Januser/Sertex.java
@@ -91,12 +91,13 @@ public class Sertex extends Wertex {
     }
     
   /** TBD */
-  public List<String[]> lookUp(String[] rowkeys) {
+  public static List<String[]> lookUp(String[] rowkeys,
+                                      String[] rowkeyNames) {
     List<String[]> existingRowkeys = new ArrayList<>();
-    if (rowkeys != null && rowkeys.length == rowkeyNames().length) { // TBD: error if not
+    if (rowkeys != null && rowkeys.length == rowkeyNames.length) { // TBD: error if not
       Map<String, String> searchMap = new HashMap<>();
-      for (int i = 0; i < rowkeyNames().length; i++) {
-        searchMap.put(rowkeyNames()[i], rowkeys[i]);
+      for (int i = 0; i < rowkeyNames.length; i++) {
+        searchMap.put(rowkeyNames[i], rowkeys[i]);
         }
       String filter = String.join(",", rowkeys);
       Map<String, Map<String, String>> results = _client.scan(null, searchMap, filter, 0, 0, false, true);
