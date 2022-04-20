dabf2f69cba7c127a97d8522311c0538fea61ce3
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-04-20 17:20:52 +0200

    ...

---
+++ src/java/com/Lomikel/Apps/LUC.java
@@ -0,0 +1,81 @@
+package com.Lomikel.Apps;
+
+import com.Lomikel.Utils.Init;
+import com.Lomikel.Utils.Info;
+import com.Lomikel.Apps.CLI;
+import com.Lomikel.Apps.BSCLI;
+import com.Lomikel.Apps.GCLI;
+import com.Lomikel.GUI.Icons;
+import com.Lomikel.Utils.StringResource;
+import com.Lomikel.Utils.LomikelException;
+
+// Bean Shell
+import bsh.Interpreter;
+import bsh.EvalError;
+
+// CLI
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLine;
+
+// Swing
+import javax.swing.ImageIcon;
+
+// Log4J
+import org.apache.log4j.Logger;
+
+/** Simple Command Line.
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public class LUC {
+
+  /** Create. */
+  public LUC() {
+    }
+
+  /** Start and pass arguments on.
+    * @param args The arguments. */
+  public static void main(String[] args) {
+    System.out.println(doit(args));
+    }
+    
+  /** Start and pass arguments on.
+    * @param args The arguments. */
+  public static String doit(String[] args) {
+    Init.init();
+    CLI.parseArgs(args, "java -jar Lomikel.exe.jar");
+    if (CLI.api().equals("bsh") ) {
+      log.info("Starting Lomikel Universal Client in BeanShell");
+      _cli = new BSCLI(Icons.lomikel,
+                       "<html><h3>http://cern.ch/hrivnac/Activities/Packages/Lomikel</h3></html>",
+                       "Welcome to Lomikel Universal Client " + Info.release() + "\nhttp://cern.ch/hrivnac/Activities/Packages/Lomikel\n" + CLI.help(),
+                       null,
+                       null);
+      }
+    else if (CLI.api().equals("groovy") ) {
+      log.info("Starting Lomikel Universal Client in Groovy");
+      _cli = new GCLI(null,
+                      null);
+      }
+    else {
+      log.fatal("Unknown api language " + CLI.api());
+      return "FATAL: Unknown api language " + CLI.api();
+      }
+    return _cli.execute();
+    }
+    
+  /** Give the embedded {@link CLI}.
+    * @return The embedded {@link CLI}. */
+  public static CLI cli() {
+    return _cli;
+    }
+    
+  private static CLI _cli;
+    
+  /** Logging . */
+  private static Logger log = Logger.getLogger(LUC.class);
+   
+ 
+  }
---
+++ src/ws/LUC.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="application/json; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<%@ page import="com.Lomikel.Apps.LUC" %>
+<%@ page import="java.net.URLDecoder" %>
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="org.apache.log4j.Logger" %>
+
+<%! static Logger log = Logger.getLogger(LUC_jsp.class); %>
+
+<%
+  String help = request.getParameter("help");
+  String api  = request.getParameter("api");
+  List<String> argsL = new ArrayList<>();
+  argsL.add("--web");
+  if (help != null) {
+    argsL.add("--help");
+    }
+  if (api != null) {
+    argsL.add("--api");
+    argsL.add(api);
+    }
+  String[] args = argsL.toArray(new String[0]);
+  if (help != null) {
+    // TBD: convert into WS format
+    out.println(LUC.cli().help());
+    }
+  out.println(LUC.doit(args));
+  LUC.cli().close();
+  %>
+
+
