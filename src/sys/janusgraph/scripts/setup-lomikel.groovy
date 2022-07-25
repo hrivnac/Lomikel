f048932efe6d79c31d95ab32cbe26750eb6a4ae5
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-07-25 13:40:43 +0200

    ...

--- src/sys/janusgraph/scripts/setup-lomikel.groovy
+++ src/sys/janusgraph/scripts/setup-lomikel.groovy
@@ -1,3 +1,52 @@
+class Lomikel {
+
+  def static hi() {
+    return "Hello World from Lomikel server !"
+    }
+    
+  def static get_or_create(lbl, name, value) {
+    return g.V().has('lbl', lbl).
+                 has(name, value).
+                 fold().
+                 coalesce(unfold(), addV(lbl).
+                                    property('lbl', lbl).
+                                    property(name, value));
+    }
+               
+  def static get_or_create_edge(g, lbl1, name1, value1, lbl2, name2, value2, edge) {
+    return g.V().has('lbl', lbl1).
+                 has(name1, value1).
+                 as('v').
+             V().has('lbl', lbl2).
+                 has(name2, value2).
+             coalesce(__.inE(edge).where(outV().as('v')), addE(edge).from('v'));
+      }
+  
+  def static getDataLink(v) {
+    switch (v.values('technology').next()) {
+      case 'HBase':
+        return 'HBase'
+        break
+      case 'Graph':
+        (backend, hostname, port, table) = v.values('url').next().split(':') // hbase:188.184.87.217:8182:janusgraph
+        graph = JanusGraphFactory.build().
+                                  set("storage.backend",     backend).
+                                  set("storage.hostname",    hostname).
+                                  set("storage.port",        port).
+                                  set("storage.hbase.table", table).
+                                  open()
+        g = graph.traversal()
+        return Eval.me('g', g, daba[3])
+        break
+      case 'Phoenix':
+        return Sql.newInstance(v.values('url').next(), 'org.apache.phoenix.jdbc.PhoenixDriver').
+                   rows(v.values('query').next())
+        break
+      default:
+        return 'unknown DataLink ' + v
+        }
+      }
+    
 def globals = [:]
 
 globals << [hook : [
@@ -8,43 +57,5 @@ globals << [hook : [
 globals << [graph : JanusGraphFactory.build().set("storage.backend", "hbase").set("storage.hostname", "@STORAGE.HOSTNAME@").set("storage.port", "@STORAGE.PORT@").set("storage.hbase.table", "@STORAGE.JANUS.TABLE@").open()]
 globals << [g : graph.traversal()]
 
-def hi_lomikel() {
-  return "Hello World from Lomikel server !"
-  }
-  
-def get_or_create(lbl, name, value) {
-  return g.V().has('lbl', lbl).
-               has(name, value).
-               fold().
-               coalesce(unfold(), addV(lbl).
-                                  property('lbl', lbl).
-                                  property(name, value));
-  }
-             
-def get_or_create_edge(g, lbl1, name1, value1, lbl2, name2, value2, edge) {
-  return g.V().has('lbl', lbl1).
-               has(name1, value1).
-               as('v').
-           V().has('lbl', lbl2).
-               has(name2, value2).
-           coalesce(__.inE(edge).where(outV().as('v')), addE(edge).from('v'));
-    }
+Lomikel.init()
 
-def getDataLink(v) {
-  switch (v.values('technology').next()) {
-    case 'HBase':
-      println 'HBase'
-      break
-    case 'Graph':
-      println 'Graph'
-      break
-    case 'Phoenix':
-      import groovy.sql.Sql
-      return Sql.newInstance(v.values('url').next(), 'org.apache.phoenix.jdbc.PhoenixDriver')
-                .firstRow(v.values('query').next())
-      break
-    default:
-      return 'unknown DataLink ' + v
-      }
-    }
-    
