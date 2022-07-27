4a5e9b3a8730ab007917b5382543e76163ec4a73
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-07-27 10:37:40 +0200

    ...

--- src/sys/janusgraph/scripts/setup-lomikel.groovy
+++ src/sys/janusgraph/scripts/setup-lomikel.groovy
@@ -1,3 +1,13 @@
+def globals = [:]
+
+globals << [hook : [
+  onStartUp: { ctx -> ctx.logger.info("Executed once at startup of Gremlin Server.")},
+  onShutDown: { ctx -> ctx.logger.info("Executed once at shutdown of Gremlin Server.")}
+  ] as LifeCycleHook]
+  
+globals << [graph : JanusGraphFactory.build().set("storage.backend", "hbase").set("storage.hostname", "@STORAGE.HOSTNAME@").set("storage.port", "@STORAGE.PORT@").set("storage.hbase.table", "@STORAGE.JANUS.TABLE@").open()]
+globals << [g : graph.traversal()]
+
 class Lomikel_CERN {
 
   def static init() {
@@ -54,16 +64,6 @@ class Lomikel_CERN {
       }
         
     }
-    
-def globals = [:]
-
-globals << [hook : [
-  onStartUp: { ctx -> ctx.logger.info("Executed once at startup of Gremlin Server.")},
-  onShutDown: { ctx -> ctx.logger.info("Executed once at shutdown of Gremlin Server.")}
-  ] as LifeCycleHook]
-  
-globals << [graph : JanusGraphFactory.build().set("storage.backend", "hbase").set("storage.hostname", "@STORAGE.HOSTNAME@").set("storage.port", "@STORAGE.PORT@").set("storage.hbase.table", "@STORAGE.JANUS.TABLE@").open()]
-globals << [g : graph.traversal()]
 
 LomikelCERN.init()
 
