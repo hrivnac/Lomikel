0d7ded02f215a71e8fe29844b6d873485f1e9dc9
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-10-12 10:13:25 +0200

    ...

--- src/ws/d3/correlogram.js
+++ src/ws/d3/correlogram.js
@@ -25,8 +25,8 @@ function showCorrelogram(otable) {
     otable.push({x:d, y:d, value:0, info:""})
     }
 
-  const width0 = dim * 80 + 30;
-  const height0 = dim * 80 + 30;
+  const width0 = dim * 40 + 30; // 430
+  const height0 = dim * 40 + 30; // 430
   const margin = { top:(height0 / num / 2),
                    right:(width0 / num / 2 + 50),
                    bottom:(height0 / num / 2),
