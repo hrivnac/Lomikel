1d474eef75be1c58d3868ca1ab55b6d630358278
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-10-26 15:05:40 +0200

    ...

--- src/java/com/Lomikel/Januser/GremlinRecipies.java
+++ src/java/com/Lomikel/Januser/GremlinRecipies.java
@@ -373,7 +373,7 @@ public class GremlinRecipies {
           evaluator.setVariable(var, new String[]{entry1.getValue().get(var).toString(), entry2.getValue().get(var).toString()});
           }
         try {
-          score = evaluator.evalDouble(null, formula);
+          score = evaluator.evalDouble(new HashMap<String, String>(), formula);
           }
         catch (LomikelException e) {
           log.error("Cannot evaluate " + formula, e);
