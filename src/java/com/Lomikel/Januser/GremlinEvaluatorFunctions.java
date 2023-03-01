34362cb5a4d8d3c873568290e70a3741c288d693
Julius Hrivnac <julius.hrivnac@cern.ch>
2023-03-01 15:12:03 +0100

    ...

--- src/java/com/Lomikel/Januser/GremlinEvaluatorFunctions.java
+++ src/java/com/Lomikel/Januser/GremlinEvaluatorFunctions.java
@@ -21,14 +21,22 @@ public class GremlinEvaluatorFunctions {
     return true;
     }
   
-  /** Give quadratic   distance of two points in multiple dimensions.
+  /** Give quadratic distance of two points in multiple dimensions.
     * @param values   The array of values of all coordinates for two points.
     *                 <code>{{x1_of_point1, x1_of_point2}, {x2_of_point1, x2_of_point2},...}</code>.
+    * @param maxSize  The max size of the value array. Full array taken if maxSize = 0 or negative.
     * @param weighted Whether the distance should be weighted by the number of dimensions.
     * @return         The quadratic distance of two points in multiple dimensions. */
   public static double qdistance(double[][] values,
+                                 int        maxSize,
                                  boolean    weighted) {
-    double distance = Arrays.stream(values).mapToDouble(d -> Math.pow(d[0] - d[1], 2)).sum();
+    double distance;
+    if (maxSize > 0) {
+      distance = Arrays.stream(values).limit(maxSize).mapToDouble(d -> Math.pow(d[0] - d[1], 2)).sum();
+      }
+    else {
+      distance = Arrays.stream(values).mapToDouble(d -> Math.pow(d[0] - d[1], 2)).sum();
+      }
     if (weighted) {
       distance = distance / values.length;
       }
@@ -38,11 +46,19 @@ public class GremlinEvaluatorFunctions {
   /** Give abs-linear distance of two points in multiple dimensions.
     * @param values   The array of values of all coordinates for two points.
     *                 <code>{{x1_of_point1, x1_of_point2}, {x2_of_point1, x2_of_point2},...}</code>.
+    * @param maxSize  The max size of the value array. Full array taken if maxSize = 0 or negative.
     * @param weighted Whether the distance should be weighted by the number of dimensions.
     * @return         The abs-linear distance of two points in multiple dimensions. */
   public static double ldistance(double[][] values,
+                                 int        maxSize,
                                  boolean    weighted) {
-    double distance = Arrays.stream(values).mapToDouble(d -> Math.abs(d[0] - d[1])).sum();
+    double distance;
+    if (maxSize > 0) {
+      distance = Arrays.stream(values).limit(maxSize).mapToDouble(d -> Math.abs(d[0] - d[1])).sum();
+      }
+    else {
+      distance = Arrays.stream(values).mapToDouble(d -> Math.abs(d[0] - d[1])).sum();
+      }
     if (weighted) {
       distance = distance / values.length;
       }
