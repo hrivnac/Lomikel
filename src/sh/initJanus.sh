02f08852a991d36fe059577bb61f16306d741a94
Julius Hrivnac <julius.hrivnac@cern.ch>
2023-07-04 21:46:20 +0200

    ...

--- src/sh/initJanus.sh
+++ src/sh/initJanus.sh
@@ -3,4 +3,3 @@ source setup.sh
 gremlin_console_Local < ../src/gremlin/schema.gremlin
 gremlin_console_Local < ../src/gremlin/indexHB.gremlin
 gremlin_console_Local < ../src/gremlin/indexES.gremlin
-gremlin_console_Local < ../src/gremlin/testGraph.gremlin
