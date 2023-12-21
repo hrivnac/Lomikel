48224f4449db9565cad9540e26d413f120803990
Julius Hrivnac <julius.hrivnac@cern.ch>
2023-12-21 10:45:14 +0100

    ...

--- src/sh/cron/uploadGraph.sh
+++ src/sh/cron/uploadGraph.sh
@@ -3,7 +3,7 @@ Y=${1}
 M=${2}
 D=${3}
 LOG=/tmp/upload.${Y}.${M}.${D}
-cd ~/FinkBrowser/ant
+cd ~/Lomikel/ant
 source ./setup.sh
 for X in 0 1 2 3 4 5 6 7 8 9; do
   /bin/rm -f ${LOG}${X}.log
--- src/sh/cron/uploadPCA.sh
+++ src/sh/cron/uploadPCA.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash -l
 LOG=/tmp/uploadPCA
-cd ~/FinkBrowser/ant
+cd ~/Lomikel/ant
 source ./setup.sh
 /bin/rm -f ${LOG}.log
 ../src/sh/importAvroHDFS-IJCLab.sh /user/julien.peloton/archive_avro/pca pca | tee ${LOG}.log 2>&1
