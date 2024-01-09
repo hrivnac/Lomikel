0f03fcebc9310d0c241d8cc6742296959cd8d0b5
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-01-09 17:26:39 +0100

    ...

---
+++ src/java/com/Lomikel/Apps/GConsole.java
@@ -0,0 +1,37 @@
+package com.Lomikel.Apps;
+
+// Groovy
+import groovy.lang.GroovyShell;
+import groovy.lang.Binding;
+import groovy.ui.Console;
+
+// Log4J
+import org.apache.log4j.Logger;
+
+/** Groovy {@link Console} as {@linki Runnable}
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public final class GConsole extends Console implements Runnable {
+
+  /** TBD */
+  public GConsole(Binding binding) {
+    super(binding);
+    }
+
+  /** TBD */
+  public GConsole() {
+    super();
+    }
+  
+  @Override
+  public void run() {
+    super.run();
+    }
+  
+  /** Logging . */
+  private static Logger log = Logger.getLogger(Console.class);
+
+  }
