cddc288ce52c858c6f32a8dd69e85e2a942753e2
Cloud User <centos@tomcat.lal.in2p3.fr>
2024-01-22 21:24:32 +0100

    ...

--- src/ws/FinkBrowser/Stylesheet.js
+++ src/ws/FinkBrowser/Stylesheet.js
@@ -311,7 +311,7 @@ stylesheet.edges.contains = {
     title:" ",
     subtitle:" ",
     arrows:{to:{enabled:true, type:"vee"}},
-    value:{gremlin:"values('instances').next().toString().split('_').length"},        
+    value:{gremlin:"values('weight').next()"},        
     group:" "
     },
   actions: [
