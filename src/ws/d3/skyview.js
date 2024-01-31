d5d2e16f74c9a0cc6f00adbed0197c3ef61c651b
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-01-31 10:09:07 +0100

    ...

--- src/ws/d3/skyview.js
+++ src/ws/d3/skyview.js
@@ -53,9 +53,9 @@ function showSkyView(data, name, url) {
     var d = data[i];
     var info = name + " " + (d.k ? d.k : "") + "(" + d.x + ", " + d.y +")";
     var size = (d.z ? 50 - (100 - 50) * (d.z - zmin) / (zmax - zmin) : 50); // TBD: better
-    var color = (d.g ? colors[d.g % 10] : 'black');
+    var color = (d.g ? colors[d.g % 10] : 'red');
     console.log(d.z + " " + size);
-    console.log(d.g + " " + color);
+    console.log(d.g + " " + (d.g % 10) + " " + color);
     features.push({"properties": {"info": info, "dim": size, "color": color},
                    "geometry": {"type": "Point", "coordinates": [d.x, d.y]}});
     };
