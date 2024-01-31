9d9b9b438f565ea001280b26852d06409fc6b2e0
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-01-31 14:38:55 +0100

    ...

--- src/ws/FinkBrowser/d3/actions.js
+++ src/ws/FinkBrowser/d3/actions.js
@@ -1,6 +1,10 @@
 function actions(url, key) {
   
-  console.log(url);
+  if (url == "") {
+    url = "http://134.158.75.151:24000";
+    }
+  
+  objectId = '';
   for (w of key.split(' ')) {
     ww = w.split('=');
     if (ww[0] == 'objectId') {
@@ -8,8 +12,8 @@ function actions(url, key) {
       }
     }
     
-  if (objectId) {
-    return "<a href='http://134.158.75.151:24000/" + key + "' title='analyse with Fink Science Portal' target='_blank'>Analyse&#8599;</a>";
+  if (objectId != "") {
+    return "<a href='" + url + "/" + objectId + "' title='analyse with Fink Science Portal' target='_blank'>Analyse&#8599;</a>";
     }
   else {
     return "xxx";
