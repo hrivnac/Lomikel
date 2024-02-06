d76d4b84824066787e5282a4a3caa48c9477ba05
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-02-06 12:44:59 +0100

    ...

--- src/ws/d3/correlogram.js
+++ src/ws/d3/correlogram.js
@@ -87,6 +87,134 @@ function showCorrelogram(otable, vennPopupWindow) {
                            
   cor.filter(function(d) {const ypos = domain.indexOf(d.y);
                           const xpos = domain.indexOf(d.x);
+                          return xpos < ypos;
+                          })
+     .append("html")
+     .attr("dy", "-2em")
+     .attr("valid", function(d) {return domain.indexOf(d.x) != domain.indexOf(d.y) && d.info != ""})
+     .attr("info",  function(d) {return "<center><b><u>" + d.y + "<br/>=><br/>" + d.x + "</u></b></br>" +
+                                        "intersection/sizeIn/sizeOut = " + d.value + "/" + d.info + "</center>"})
+     .attr("popx",  function(d) {return x(d.x)})
+     .attr("popy",  function(d) {return y(d.y)})
+     .text(function(d) {return d.x})
+     .style("font-size", 8)
+     .style("text-align", "center")
+     .style("fill", function(d) {return color(d3.select(this).attr("p12"))})
+     .on("mouseover", function(d) {	
+            var g = d3.select(this);
+            var div = d3.select("body").append("div")
+                    .attr('pointer-events', 'none')
+                    .attr("class", "tooltip")
+                    .style("opacity", 1)
+                    .html("FIRST LINE <br> SECOND LINE")
+                    .style("left", (x(d.x) + 50 + "px"))
+                    .style("top", (y(d.y) +"px"));
+          });
+     
+  //cor.filter(function(d) {const ypos = domain.indexOf(d.y);
+  //                        const xpos = domain.indexOf(d.x);
+  //                        return xpos < ypos;
+  //                        })
+  //   .append("text")
+  //   .attr("dy", "-1em")
+  //   .attr("m1",    function(d) {return d.x})
+  //   .attr("m2",    function(d) {return d.y})
+  //   .attr("n1",    function(d) {return d.info.split("/")[0]})
+  //   .attr("n2",    function(d) {return d.info.split("/")[1]})
+  //   .attr("n12",   function(d) {return d.value})
+  //   .attr("p12",   function(d) {return 100 * parseInt(d.value) / (parseInt(d3.select(this).attr("n1")) + parseInt(d3.select(this).attr("n2")) - parseInt(d.value))})
+  //   .attr("valid", function(d) {return domain.indexOf(d.x) != domain.indexOf(d.y) && d.info != ""})
+  //   .attr("info",  function(d) {return "<center><b><u>" + d.y + "<br/>=><br/>" + d.x + "</u></b></br>" +
+  //                                      "intersection/sizeIn/sizeOut = " + d.value + "/" + d.info + "</center>"})
+  //   .attr("popx",  function(d) {return x(d.x)})
+  //   .attr("popy",  function(d) {return y(d.y)})
+  //   .text(function(d) {return d.x})
+  //   .style("font-size", 8)
+  //   .style("text-align", "center")
+  //   .style("fill", function(d) {return color(d3.select(this).attr("p12"))})
+  //   .on("mouseover", function(d) {	
+  //        if (d3.select(this).attr("valid")) {
+  //          info = showVenn(d3.select(this).attr("n1"),
+  //                          d3.select(this).attr("n2"),
+  //                          d3.select(this).attr("n12"),
+  //                          d3.select(this).attr("m1").replaceAll('.', ' '),
+  //                          d3.select(this).attr("m2").replaceAll('.', ' '));
+  //          document.getElementById("vennTxt").innerHTML = info;
+  //          }
+  //        });
+                           
+  //cor.filter(function(d) {const ypos = domain.indexOf(d.y);
+  //                        const xpos = domain.indexOf(d.x);
+  //                        return xpos < ypos;
+  //                        })
+  //   .append("text")
+  //   .attr("dy", "1em")
+  //   .attr("m1",    function(d) {return d.x})
+  //   .attr("m2",    function(d) {return d.y})
+  //   .attr("n1",    function(d) {return d.info.split("/")[0]})
+  //   .attr("n2",    function(d) {return d.info.split("/")[1]})
+  //   .attr("n12",   function(d) {return d.value})
+  //   .attr("p12",   function(d) {return 100 * parseInt(d.value) / (parseInt(d3.select(this).attr("n1")) + parseInt(d3.select(this).attr("n2")) - parseInt(d.value))})
+  //   .attr("valid", function(d) {return domain.indexOf(d.x) != domain.indexOf(d.y) && d.info != ""})
+  //   .attr("info",  function(d) {return "<center><b><u>" + d.y + "<br/>=><br/>" + d.x + "</u></b></br>" +
+  //                                      "intersection/sizeIn/sizeOut = " + d.value + "/" + d.info + "</center>"})
+  //   .attr("popx",  function(d) {return x(d.x)})
+  //   .attr("popy",  function(d) {return y(d.y)})
+  //   .text(function(d) {return d.value})
+  //   .style("font-size", 8)
+  //   .style("text-align", "center")
+  //   .style("fill", function(d) {return color(d3.select(this).attr("p12"))})
+  //   .on("mouseover", function(d) {	
+  //        if (d3.select(this).attr("valid")) {
+  //          info = showVenn(d3.select(this).attr("n1"),
+  //                          d3.select(this).attr("n2"),
+  //                          d3.select(this).attr("n12"),
+  //                          d3.select(this).attr("m1").replaceAll('.', ' '),
+  //                          d3.select(this).attr("m2").replaceAll('.', ' '));
+  //          document.getElementById("vennTxt").innerHTML = info;
+  //          }
+  //        });
+     
+  //cor.filter(function(d) {const ypos = domain.indexOf(d.y);
+  //                        const xpos = domain.indexOf(d.x);
+  //                        return xpos < ypos;
+  //                        })
+  //   .append("text")
+  //   .attr("dy", "2em")
+  //   .attr("m1",    function(d) {return d.x})
+  //   .attr("m2",    function(d) {return d.y})
+  //   .attr("n1",    function(d) {return d.info.split("/")[0]})
+  //   .attr("n2",    function(d) {return d.info.split("/")[1]})
+  //   .attr("n12",   function(d) {return d.value})
+  //   .attr("p12",   function(d) {return 100 * parseInt(d.value) / (parseInt(d3.select(this).attr("n1")) + parseInt(d3.select(this).attr("n2")) - parseInt(d.value))})
+  //   .attr("valid", function(d) {return domain.indexOf(d.x) != domain.indexOf(d.y) && d.info != ""})
+  //   .attr("info",  function(d) {return "<center><b><u>" + d.y + "<br/>=><br/>" + d.x + "</u></b></br>" +
+  //                                      "intersection/sizeIn/sizeOut = " + d.value + "/" + d.info + "</center>"})
+  //   .attr("popx",  function(d) {return x(d.x)})
+  //   .attr("popy",  function(d) {return y(d.y)})
+  //   .text(function(d) {return d.info})
+  //   .style("font-size", 8)
+  //   .style("text-align", "center")
+  //   .style("fill", function(d) {return color(d3.select(this).attr("p12"))})
+  //   .on("mouseover", function(d) {	
+  //        if (d3.select(this).attr("valid")) {
+  //          info = showVenn(d3.select(this).attr("n1"),
+  //                          d3.select(this).attr("n2"),
+  //                          d3.select(this).attr("n12"),
+  //                          d3.select(this).attr("m1").replaceAll('.', ' '),
+  //                          d3.select(this).attr("m2").replaceAll('.', ' '),
+  //                          vennPopupWindow);
+  //          if (vennPopupWindow) {
+  //            vennPopupWindow.document.getElementById("vennPopupTxt").innerHTML = info;
+  //            }
+  //          else {
+  //            document.getElementById("vennTxt").innerHTML = info;
+  //            }
+  //          }
+  //        });
+      
+  cor.filter(function(d) {const ypos = domain.indexOf(d.y);
+                          const xpos = domain.indexOf(d.x);
                           return xpos > ypos;
                           })
      .append("circle")
