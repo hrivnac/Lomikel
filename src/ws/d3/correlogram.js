82859d00d5f65c505229b53b95b18666d49fb739
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-02-06 12:53:35 +0100

    ...

--- src/ws/d3/correlogram.js
+++ src/ws/d3/correlogram.js
@@ -66,11 +66,11 @@ function showCorrelogram(otable, vennPopupWindow) {
                  .attr("class", "cor")
                  .attr("transform", function(d) {return `translate(${x(d.x)}, ${y(d.y)})`});
 
-  cor.append("rect")
-     .attr("width",  xSpace / (num - 1))
-     .attr("height", ySpace / (num - 1))
-     .attr("x",     -xSpace / (num - 1) / 2)
-     .attr("y",     -ySpace / (num - 1) / 2)                 
+//  cor.append("rect")
+//     .attr("width",  xSpace / (num - 1))
+//     .attr("height", ySpace / (num - 1))
+//     .attr("x",     -xSpace / (num - 1) / 2)
+//     .attr("y",     -ySpace / (num - 1) / 2)                 
       
 //  for (var i = 2; i < 7; i++) {
 //    cor.filter(function(d) {const ypos = domain.indexOf(d.y);
