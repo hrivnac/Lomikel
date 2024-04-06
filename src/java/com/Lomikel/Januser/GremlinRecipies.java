403b096fc3f8229cbea98b3999e2bd1d4e26ff22
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-04-06 22:42:12 +0200

    ...

--- src/java/com/Lomikel/Januser/GremlinRecipies.java
+++ src/java/com/Lomikel/Januser/GremlinRecipies.java
@@ -274,7 +274,7 @@ public class GremlinRecipies {
       }
     if (!create && reset) {
       List<Edge> edges = getEdge(v1, v2, relation);
-      if (edges.size() > 0) {
+      if (edges.size() != 1) {
         log.error("" + edges.size() + " edges exists, none modified");
         }
       else {
