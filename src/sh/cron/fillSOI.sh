6724ddac7bebbee6b71f33d50735ce16e0710d77
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-04-07 09:44:24 +0200

    ...

--- src/sh/cron/fillSOI.sh
+++ src/sh/cron/fillSOI.sh
@@ -10,6 +10,6 @@ PID=$$
 echo ${PID} > ${LOCK}
 cd ~/Lomikel/ant
 source ./setup.sh
-echo "new com.astrolabsoftware.FinkBrowser.Januser.FinkGremlinRecipies(g).processSourcesOfInterest('hbase-1.lal.in2p3.fr:2183:ztf:schema_2.2_2.0.0', 10000, 10000, false, false, null)" | CLASSPATH="${GREMLIN_CLASSPATH}" ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_console.gremlin ${janusgraph_dir}/conf/gremlin-server/Local-IJCLab.properties ${home}" | tee -a ${LOG} 2>&1
-echo "new com.astrolabsoftware.FinkBrowser.Januser.FinkGremlinRecipies(g).processSourcesOfInterest('hbase-1.lal.in2p3.fr:2183:ztf:schema_2.2_2.0.0', 10000, 10000, true,  false, null)" | CLASSPATH="${GREMLIN_CLASSPATH}" ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_console.gremlin ${janusgraph_dir}/conf/gremlin-server/Local-IJCLab.properties ${home}" | tee -a ${LOG} 2>&1
+#echo "new com.astrolabsoftware.FinkBrowser.Januser.FinkGremlinRecipies(g).processSourcesOfInterest('hbase-1.lal.in2p3.fr:2183:ztf:schema_2.2_2.0.0', 10000, 10000, false, false, null)" | CLASSPATH="${GREMLIN_CLASSPATH}" ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_console.gremlin ${janusgraph_dir}/conf/gremlin-server/Local-IJCLab.properties ${home}" | tee -a ${LOG} 2>&1
+echo "new com.astrolabsoftware.FinkBrowser.Januser.FinkGremlinRecipies(g).processSourcesOfInterest('hbase-1.lal.in2p3.fr:2183:ztf:schema_2.2_2.0.0', 1000, 10000, true,  false, null)" | CLASSPATH="${GREMLIN_CLASSPATH}" ${janusgraph_dir}/bin/gremlin.sh -i "../src/gremlin/start_console.gremlin ${janusgraph_dir}/conf/gremlin-server/Local-IJCLab.properties ${home}" | tee -a ${LOG} 2>&1
 /bin/rm -f ${LOCK} 
