9ae6827754461103d7523ce09c6a3cc5b35d4ca1
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-04-18 13:22:00 +0200

    ...

--- src/groovy/com/astrolabsoftware/FinkBrowser/Januser/FinkGremlinRecipiesG.groovy
+++ src/groovy/com/astrolabsoftware/FinkBrowser/Januser/FinkGremlinRecipiesG.groovy
@@ -52,7 +52,8 @@ import org.apache.logging.log4j.LogManager;
   * @opt types
   * @opt visibility
   * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
-class FinkGremlinRecipiesG extends FinkGremlinRecipies {
+public class FinkGremlinRecipiesG extends FinkGremlinRecipies
+                                  implements FinkGremlinRecipiesGT {
   
   /** Create and attach to {@link GraphTraversalSource}.
     * @param g The attached {@link GraphTraversalSource}. */
@@ -66,112 +67,6 @@ class FinkGremlinRecipiesG extends FinkGremlinRecipies {
     super(client);
     }
     
-  /** Give {@link Map} of other <em>source</em>s ordered
-    * by distance to the specified <em>source</em> with respect
-    * to weights to all (or selected) <em>SourceOfInterest</em> classes.
-    * @param oid0      The <em>objectOd</em> of the <em>source</em>.
-    * @param oidS      An array of <em>source</em> objectIds to only avaluated.
-    *                  If null, all <em>source</em>s will be evaluated.
-    * @param classes0A An array of <em>SourceOfInterest</em> classes to be
-    *                  used in comparison.
-    *                  All <em>SourceOfInterest</em> classes of thr specified
-    *                  <em>source</em> will be used if <tt>null</tt>.
-    * @param nmax      The number of closest <em>source</em>s to give.
-    * @return          The distances to other sources, order by the distance. */
-  public Map<String, Double> sourceNeighborhood(String   oid0,
-                                                String[] oidS,
-                                                String[] classes0,
-                                                int      nmax) {
-    return sourceNeighborhood(oid0, Arrays.asList(oidS), Arrays.asList(classes0), nmax);
-    }
-    
-  /** Give {@link Map} of other <em>source</em>s ordered
-    * by distance to the specified <em>source</em> with respect
-    * to weights to all (or selected) <em>SourceOfInterest</em> classes.
-    * @param oid0      The <em>objectOd</em> of the <em>source</em>.
-    * @param oidS      A {@link List} of <em>source</em> objectIds to only avaluated.
-    *                  If null, all <em>source</em>s will be evaluated.
-    * @param classes0A A {@link List} of <em>SourceOfInterest</em> classes to be
-    *                  used in comparison.
-    *                  All <em>SourceOfInterest</em> classes of thr specified
-    *                  <em>source</em> will be used if <tt>null</tt>.
-    * @param nmax      The number of closest <em>source</em>s to give.
-    * @return          The distances to other sources, order by the distance. */
-  public Map<String, Double> sourceNeighborhood(String       oid0,
-                                                List<String> oidS,
-                                                List<String> classes0,
-                                                int          nmax) {
-    def source0 = g().V().has('lbl', 'source').has('objectId', oid0).next();
-    def m0 = [:];
-    g().V(source0).inE().
-                   project('cls', 'w').
-                   by(outV().values('cls')).
-                   by(values('weight')).each {it ->
-                                              if (classes0 == null || it['cls'] in classes0) {
-                                                m0[it['cls']] = it['w'];
-                                                }
-                                              }
-    def classes = [];
-    for (entry : m0.entrySet()) {
-      classes += [entry.getKey()];
-      }
-    def n0 = m0.size();
-    for (entry : m0.entrySet()) {
-      m0[entry.getKey()] = entry.getValue() / n0;
-      }
-    log.info('calculating source distance from ' + oid0 + ' wrt ' + classes + " ...");
-    def distances = [:]
-    def sources;
-    if (oidS) {
-      log.info("\tsearching only " + oidS);
-      sources = g().V().has('lbl', 'source').has('objectId', within(oidS));
-      }
-    else {
-      sources = g().V().has('lbl', 'source');   
-      }
-    sources.each {s -> 
-                  def oid = g().V(s).values('objectId').next();
-                  def m = [:];
-                  g().V(s).inE().
-                           project('cls', 'w').
-                           by(outV().values('cls')).
-                           by(values('weight')).each {it ->
-                                                      if (it['cls'] in classes) {
-                                                        m[it['cls']] = it['w'];
-                                                        }
-                                                      }
-                  def n = m.size();
-                  for (entry : m.entrySet()) {
-                    m[entry.getKey()] = entry.getValue() / n;
-                    }
-                  def dist = 0;
-                  def w01;
-                  def w02;
-                  def wx1;
-                  def wx2;
-                  def w0;
-                  def wx;
-                  for (entry1 : m0.entrySet()) {
-                    for (entry2 : m0.entrySet()) {
-                      if (entry1.getKey() > entry2.getKey()) {
-                        w01 = entry1.getValue();
-                        w02 = entry2.getValue();
-                        wx1 = m[entry1.getKey()] == null ? 0 : m[entry1.getKey()];
-                        wx2 = m[entry2.getKey()] == null ? 0 : m[entry2.getKey()];
-                        w0 = (w01 + w02) == 0 ? 0 : Math.abs(w01 - w02) / (w01 + w02);
-                        wx = (wx1 + wx2) == 0 ? 0 : Math.abs(wx1 - wx2) / (wx1 + wx2);
-                        dist += Math.pow(w0 - wx, 2);
-                        }
-                      }
-                    }
-                  dist = Math.sqrt(dist) / n / n;
-                  if (dist > 0) {
-                    distances[oid] = dist;
-                    }                                
-                  }   
-    return distances.sort{it.value}.take(nmax);
-    }
-
   /** Logging . */
   private static Logger log = LogManager.getLogger(FinkGremlinRecipiesG.class);
 
--- src/groovy/testJanusClient.groovy
+++ src/groovy/testJanusClient.groovy
@@ -3,4 +3,7 @@ import com.astrolabsoftware.FinkBrowser.Januser.FinkGremlinRecipiesG;
 
 jc = new JanusClient("/opt/janusgraph-1/conf/gremlin-server/IJCLab.properties");
 gr = new FinkGremlinRecipiesG(jc);
+print(jc.g().V().limit(1).valueMap().next());
 print(gr.sourceNeighborhood("ZTF17aaawgky", ["ZTF18abablgk", "ZTF18ablxmyz"], null, 10));
+print(gr.stat());
+
