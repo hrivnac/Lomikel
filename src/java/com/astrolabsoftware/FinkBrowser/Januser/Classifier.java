4262097cc5f616305e2f9b997483ac27afc11433
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-05-07 13:45:56 -0300

    ...

---
+++ src/java/com/astrolabsoftware/FinkBrowser/Januser/Classifier.java
@@ -0,0 +1,12 @@
+package com.astrolabsoftware.FinkBrowser.Januser;
+
+/** <code>Classifier</code> specifies how sources and alerts are claaaified.
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public enum Classifier {
+  FINK_PORTAL,
+  UNKNOWN;   
+  }
