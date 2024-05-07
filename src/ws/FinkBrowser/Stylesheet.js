7b01f0dd52a56d34559b4f3d08ae6eb5d2b08ecf
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-05-07 14:10:26 -0300

    ...

--- src/ws/FinkBrowser/Stylesheet.js
+++ src/ws/FinkBrowser/Stylesheet.js
@@ -227,8 +227,8 @@ stylesheet.nodes.AlertsOfInterest = {
   properties:{},
   graphics: {
     label:"cls", 
-    title:"cls",        
-    subtitle:" ",     
+    title:"classifier",        
+    subtitle:"cls",     
     group:" ",        
     shape:"image",      
     image:"StarsAlerts.png",        
@@ -246,8 +246,8 @@ stylesheet.nodes.SourcesOfInterest = {
   properties:{},
   graphics: {
     label:"cls", 
-    title:"cls",        
-    subtitle:" ",     
+    title:"classifier",        
+    subtitle:"cls",     
     group:" ",        
     shape:"image",      
     image:"StarsSources.png",        
