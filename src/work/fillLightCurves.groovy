8154877cb116d196ec8246ec388c81852afee7fb
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-05-24 14:02:47 +0200

    ...

---
+++ src/work/fillLightCurves.groovy
@@ -0,0 +1,14 @@
+import com.Lomikel.HBaser.HBaseClient;
+import com.astrolabsoftware.FinkBrowser.HBaser.FinkHBaseClient;
+source = new HBaseClient("hbase-1.lal.in2p3.fr", 2183);
+source.connect("ztf");
+source.setLimit(1000);
+oids = source.latests("i:objectId",
+                      null,
+                      1000,
+                      true);
+oids = oids.toArray().join(",");
+source.setLimit(0);
+client = new FinkHBaseClient("hbase-1.lal.in2p3.fr", 2183);
+client.connect("LightCurves", null);
+client.assembleLightCurves(source, oids);
