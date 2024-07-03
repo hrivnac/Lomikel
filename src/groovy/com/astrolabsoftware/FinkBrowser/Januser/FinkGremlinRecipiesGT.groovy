7558b2fdaff39d044a4d6dbd2beed82fae93d384
Julius Hrivnac <julius.hrivnac@cern.ch>
2024-07-03 15:34:43 +0200

    ...

--- src/groovy/com/astrolabsoftware/FinkBrowser/Januser/FinkGremlinRecipiesGT.groovy
+++ src/groovy/com/astrolabsoftware/FinkBrowser/Januser/FinkGremlinRecipiesGT.groovy
@@ -75,6 +75,20 @@ public trait FinkGremlinRecipiesGT extends GremlinRecipiesGT {
       }
     return g().V().has('direction', geoWithin(Geoshape.circle(lat, lon, dist))).limit(nDir).has('jd', inside(jdmin, jdmax)).limit(nJD);
     }
+    
+  /** Give {@link Map} of other <em>source</em>s ordered
+    * by distance to the specified <em>source</em> with respect
+    * to weights to all (or selected) <em>SourceOfInterest</em> classes.
+    * @param oid0       The <em>objectOd</em> of the <em>source</em>.
+    * @param classifier The classifier name to be used.
+    * @param nmax       The number of closest <em>source</em>s to give.
+    *                   All are given, if missing.
+    * @return           The distances to other sources, order by the distance. */
+  def  Map<String, Double> sourceNeighborhood(String   oid0,
+                                              String   classifier,
+                                              int      nmax = Integer.MAX_VALUE) {
+    return sourceNeighborhood(oid0, null, null, nmax);
+    }
       
   /** Give {@link Map} of other <em>source</em>s ordered
     * by distance to the specified <em>source</em> with respect
