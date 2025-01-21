d64bd23d1f1d2ce06d743a56fded3217a199c211
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-01-21 14:00:41 +0100

    ...

--- src/work/fillSOI.groovy
+++ src/work/fillSOI.groovy
@@ -1,8 +1,11 @@
+
+
+
 import com.Lomikel.Januser.JanusClient;
 import com.astrolabsoftware.FinkBrowser.Januser.FinkGremlinRecipies;
 
-jc = new JanusClient("/opt/janusgraph-1/conf/gremlin-server/IJCLab.properties");
-gr = new FinkGremlinRecipies(g)
+jc = new JanusClient("/opt/janusgraph-1/conf/gremlin-server/IJCLab.properties")
+gr = new FinkGremlinRecipies(jc)
 
 //gr.processSourcesOfInterest('FINK_PORTAL',    'hbase-1.lal.in2p3.fr:2183:ztf:', 1000, 10000, null, false, null)
 //gr.processSourcesOfInterest('FINK_PORTAL',    'hbase-1.lal.in2p3.fr:2183:ztf:', 1000, 10000, new String[]{'SN candidate', 'Kilonova candidate', 'Early SN Ia candidate', 'Ambiguous', '*'},  false, null)
