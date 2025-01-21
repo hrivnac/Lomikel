4660c833243599e49c02c0221d66585b5c266062
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-01-21 13:42:59 +0100

    ...

--- src/sh/cron/fillLightCurves.sh
+++ src/sh/cron/fillLightCurves.sh
@@ -3,12 +3,12 @@ NOW=`date +"%Y%m%d%H%M%s"`
 LOG=/tmp/fillLC-${NOW}.log
 LOCK=/tmp/fillLC.lock 
 if [[ -e ${LOCK} ]]; then
-  echo "Already filling LightCurves with ${LOCK}"
+  echo "Already filling ES with ${LOCK}"
   exit
   fi
 PID=$$
 echo ${PID} > ${LOCK}
 cd ~/Lomikel/ant
 source ./setup.sh
-java -jar ~/Lomikel/lib/Lomikel-HBase.exe.jar -b -s ~/Lomikel/src/work/fillLightCurves.groovy | tee -a ${LOG} 2>&1
+java -jar ~/Lomikel/lib/Lomikel-HBase.exe.jar -b -s ~/Lomikel/src/work/fillES.groovy | tee -a ${LOG} 2>&1
 /bin/rm -f ${LOCK} 
