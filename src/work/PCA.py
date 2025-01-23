a83a8343b74abd1e10cd99c663bfd76958f2a135
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-01-23 18:10:24 +0100

    ...

--- src/work/PCA.py
+++ src/work/PCA.py
@@ -32,7 +32,7 @@ def classification(objectId):
       aclassifications[objectId] = t
       classifications[objectId] = objectId
     except:
-      classifications[objectId] = "failed"
+      classifications[objectId] = "failed:" + objectId
   return classifications[objectId]
  
 classification_udf = udf(lambda x: classification(x), StringType())
