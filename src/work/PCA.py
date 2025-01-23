d719331408ff19d6de8b27ead0fe1bf312187b5c
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-01-23 15:35:26 +0100

    ...

--- src/work/PCA.py
+++ src/work/PCA.py
@@ -25,11 +25,12 @@ classifications = {}
 def classification(objectId):
   if objectId not in classifications:
     try:
-      r = requests.post("https://api.fink-portal.org/api/v1/objects",
-                        json={"objectId": objectId, "output-format": "json"})  
-      s = json.loads(r.text)  
-      t = s[0]["v:classification"]
-      classifications[objectId] = t
+      #r = requests.post("https://api.fink-portal.org/api/v1/objects",
+      #                  json={"objectId": objectId, "output-format": "json"})  
+      #s = json.loads(r.text)  
+      #t = s[0]["v:classification"]
+      #classifications[objectId] = t
+      classifications[objectId] = objectId
     except:
       classifications[objettId] = "failed"
   return classifications[objectId]
