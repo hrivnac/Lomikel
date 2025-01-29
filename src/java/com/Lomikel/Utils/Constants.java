ec9accf1c15080c6dcac0bb060db8b7c38ab2d52
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-01-29 15:08:24 +0100

    ...

---
+++ src/java/com/Lomikel/Utils/Constants.java
@@ -0,0 +1,14 @@
+package com.Lomikel.Utils;
+
+/** <code>Constants</code> carry various usefull constants.
+  * It uses unicode.
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public class Constants {
+  
+  public static double π = Math.PI;
+  
+  }
