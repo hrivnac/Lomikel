3ccd5f2db34fb80f94fa36d6d1f42d0bc362c649
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-01-30 13:21:36 +0100

    ...

---
+++ src/java/com/astrolabsoftware/FinkBrowser/Januser/FeaturesClassifier.java
@@ -0,0 +1,95 @@
+package com.astrolabsoftware.FinkBrowser.Januser;
+
+import com.Lomikel.HBaser.HBaseClient;
+import com.Lomikel.Utils.LomikelException;
+import com.astrolabsoftware.FinkBrowser.FinkPortalClient.FPC;
+
+// org.json
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+// Java
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.TreeMap;
+
+// Log4J
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+/** <code>FeaturesClassifier</code> classifies sources according to
+  * HBase <tt>lc_features_*</tt> field.
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public class FeaturesClassifier implements Classifier {
+  
+  @Override
+  public void classify(FinkGremlinRecipies recipies,
+                       String              oid,
+                       String              hbaseUrl,
+                       boolean             enhance,
+                       String              columns) throws LomikelException {
+    log.info(client(hbaseUrl));
+    //JSONArray ja;
+    //JSONObject jo;
+    //Map<String, Set<Double>> classes; // cls -> [jd]
+    //String cl;
+    //double jd;
+    //Set<Double> jds;
+    //String key;
+    //Set<Double> val;
+    //int weight;
+    //ja = FPC.objects(new JSONObject().put("objectId",      oid   ).
+    //                                  put("output-format", "json"));
+    //classes =  new TreeMap<>();
+    //// get all alerts (jd) and their classes
+    //for (int i = 0; i < ja.length(); i++) {
+    //  jo = ja.getJSONObject(i);
+    //  cl = jo.getString("v:classification");
+    //  jd = jo.getDouble("i:jd");
+    //  if (!cl.equals("Unknown")) {
+    //    if (classes.containsKey(cl)) {
+    //      jds = classes.get(cl);
+    //      jds.add(jd);
+    //      }
+    //    else {
+    //      jds = new TreeSet<Double>();
+    //      jds.add(jd);
+    //      classes.put(cl, jds);
+    //      }
+    //    }
+    //  }
+    //for (Map.Entry<String, Set<Double>> cls : classes.entrySet()) {
+    //  key = cls.getKey();
+    //  val = cls.getValue();
+    //  weight = val.size();
+    //  log.info("\t" + key + " in " + weight + " alerts");
+    //  recipies.registerSourcesOfInterest(Classifiers.FEATURES, key, oid, weight, val, hbaseUrl, enhance, columns);
+    //  }
+    }
+    
+  /** TBD */
+  private HBaseClient client(String hbaseUrl) throws LomikelException{
+    if (_client == null) {
+      String[] hbaseUrlA = hbaseUrl.split(":");
+      if (hbaseUrlA.length < 4) {
+        throw new LomikelException("Cannot create HBase client, hbaseUrl uncomplete: " + hbaseUrl);
+        }
+      _client = new HBaseClient(hbaseUrlA[0], hbaseUrlA[1]);
+      _client.connect(hbaseUrlA[2], hbaseUrlA[3]);
+      }
+    return _client;
+    }
+    
+  private static HBaseClient _client;
+
+  /** Logging . */
+  private static Logger log = LogManager.getLogger(FeaturesClassifier.class);
+  
+  }
+           
+           
