98859b291771c26c219b70f59cb7913ad85baab5
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-01-31 14:19:26 +0100

    ...

--- src/groovy/com/astrolabsoftware/FinkBrowser/Januser/FinkGremlinRecipiesGT.groovy
+++ src/groovy/com/astrolabsoftware/FinkBrowser/Januser/FinkGremlinRecipiesGT.groovy
@@ -114,8 +114,7 @@ public trait FinkGremlinRecipiesGT extends GremlinRecipiesGT {
       log.info(oid0 + " has no registered neighborhood");
       return [:];
       }
-    def classifierClasses = g().V().has('lbl', 'SourcesOfInterest').has('classifier', 'FINK_PORTAL').values('cls').toSet();
-    log.info(classifierClasses);
+    def classifierClasses = g().V().has('lbl', 'SourcesOfInterest').has('classifier', classifier).values('cls').toSet();
     def source0 = g().V().has('lbl', 'source').has('objectId', oid0).next();
     def m0 = [:];
     g().V(source0).inE().
