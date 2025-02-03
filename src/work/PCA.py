40db744b1a281a9c7f32c48f216e5fdb07ae4776
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-02-03 14:31:25 +0100

    ...

--- src/work/PCA.py
+++ src/work/PCA.py
@@ -58,7 +58,9 @@ cols = ["magpsf",
         "magzpsci"]
 df = spark.read.format("org.apache.hadoop.hbase.spark").option("hbase.columns.mapping", mapping).option("hbase.table", "ztf").option("hbase.spark.use.hbasecontext", False).option("hbase.spark.pushdown.columnfilter", True).load().filter(~F.col("rowkey").startswith("schema_")).limit(1000)
 
-df.show(truncate=False, n=1000)
+df = df.filter(df.lc_features_g != "null" && df.lc_features_r != "null" )
+
+df.show(truncate=False)
 
 ##print("*** VectorAssembler ***")
 ##vecAssembler = VectorAssembler(inputCols=cols, outputCol="features")
