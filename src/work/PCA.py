b2af815bc77b4ddbf66ffdf8dcd8f16f129a0e77
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-02-03 14:41:49 +0100

    ...

--- src/work/PCA.py
+++ src/work/PCA.py
@@ -58,7 +58,7 @@ cols = ["magpsf",
         "magzpsci"]
 df = spark.read.format("org.apache.hadoop.hbase.spark").option("hbase.columns.mapping", mapping).option("hbase.table", "ztf").option("hbase.spark.use.hbasecontext", False).option("hbase.spark.pushdown.columnfilter", True).load().filter(~F.col("rowkey").startswith("schema_")).limit(1000)
 
-df = df.filter(df.lc_features_g != None)
+df = df.filter(df.lc_features_g.isNotNull())
 
 df.show(truncate=False)
 
