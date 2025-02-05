ffd6eb513e49a4387c5976fb5319e697abdeb67c
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-02-05 11:05:54 +0100

    ...

--- src/work/PCA.py
+++ src/work/PCA.py
@@ -257,8 +257,8 @@ df_standardized = scaler_model.transform(df_vector)
 
 # PCA --------------------------------------------------------------------------
 
-vecAssembler = VectorAssembler(inputCols="scaled_features", outputCol="prePcafeatures")
-pca = PCA(k=n_pca, inputCol="prePcafeatures", outputCol="pcaFeatures")
+#vecAssembler = VectorAssembler(inputCols="scaled_features", outputCol="prePcafeatures")
+pca = PCA(k=n_pca, inputCol="scaled_features", outputCol="pcaFeatures")
 pipeline = Pipeline(stages=[vecAssembler, pca])
 model = pipeline.fit(df_standardized)
 pca_model = model.stages[1]
