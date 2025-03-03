3bb5fe794bdc707cf88ebd3791e2d4b7e0338506
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-03-03 18:48:26 +0100

    ...

--- src/work/analysePCA.py
+++ src/work/analysePCA.py
@@ -8,7 +8,7 @@ normalised = True
 classifiers1 = ['FEATURES',          'FEATURES',          'FINK_PORTAL',       'FEATURES',         'FEATURES',         'FINK_PORTAL'     ]
 classifiers2 = ['FINK_PORTAL',       'FEATURES',          'FINK_PORTAL',       'FINK_PORTAL',      'FEATURES',         'FINK_PORTAL'     ]
 types        = ['SourcesOfInterest', 'SourcesOfInterest', 'SourcesOfInterest', 'AlertsOfInterest', 'AlertsOfInterest', 'AlertsOfInterest']
-limits_norm  = [0.6,                 0.0,                 0.9,                  0.8,               0.0,                0.9               ]
+limits_norm  = [0.1,                 0.0,                 0.2,                  0.15,              0.0,                0.2               ]
 limits_unorm = [120,                 0.0,                 100,                  1200,              0.0,                10000             ]
 
 if normalised:
@@ -20,7 +20,15 @@ else:
   limits  = limits_unorm
   overlap = 'overlap'
 
+sample = pd.read_csv('PCA-sample.csv')
+cls_values = sample['cls'].unique()
+print(cls_values)
+
 df = pd.read_csv('overlaps.csv')
+df = df[((df['classifier1'] != 'FINK_PORTAL') | (df['class1'].isin(cls_values))) &
+        ((df['classifier2'] != 'FINK_PORTAL') | (df['class2'].isin(cls_values))) &
+        ((df['classifier1'] != 'FEATURES'   ) | (df['class1'] != "FC--1"      )) &
+        ((df['classifier2'] != 'FEATURES'   ) | (df['class2'] != "FC--1"      ))]
 
 fig, axes = plt.subplots(2, 3, figsize = (20, 15))  
 
@@ -32,9 +40,7 @@ for i, ax in enumerate(axes.flat):
   dfx = df.query('type1 == @type').\
            query('type2 == @type').\
            query('classifier1 == @classifier1').\
-           query('classifier2 == @classifier2').\
-           query('class1 != "FC--1"').\
-           query('class2 != "FC--1"')
+           query('classifier2 == @classifier2')
   dfx['normalized_overlap1'] = dfx.groupby('class1')['overlap'].\
                                    transform(lambda x: x / x.sum())
   dfx['normalized_overlap2'] = dfx.groupby('class2')['normalized_overlap1'].\
