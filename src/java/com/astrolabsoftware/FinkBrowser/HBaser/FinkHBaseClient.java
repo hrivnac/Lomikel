3f7246a7af3cdc1d5c3cc61ad3697ab9f2293d79
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-03-05 22:01:03 +0100

    ...

--- src/java/com/astrolabsoftware/FinkBrowser/HBaser/FinkHBaseClient.java
+++ src/java/com/astrolabsoftware/FinkBrowser/HBaser/FinkHBaseClient.java
@@ -184,7 +184,9 @@ public class FinkHBaseClient extends HBaseSQLClient {
     Map<String, String> searchMap = new TreeMap<>();
     try {
       HBaseClient client = new HBaseClient(zookeepers(), clientPort());
+      log.info(client.schema());
       client.connect(tableName() + ".jd", null);
+      log.info(client.schema());
       client.setReversed(reversed);
       client.setLimit(limit());
       client.setSearchLimit(searchLimit());
