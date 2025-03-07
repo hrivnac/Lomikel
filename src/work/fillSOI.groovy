fdc5834fe22ff2e15afb2cd549e97c9fcb9c9b62
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-03-07 14:53:26 +0100

    ...

--- src/work/fillSOI.groovy
+++ src/work/fillSOI.groovy
@@ -5,10 +5,16 @@ import com.astrolabsoftware.FinkBrowser.Januser.Classifiers;
 jc = new JanusClient("/opt/janusgraph-1/conf/gremlin-server/IJCLab.properties")
 gr = new FinkGremlinRecipies(jc)
 
-//gr.processSourcesOfInterest('FINK_PORTAL', 'hbase-1.lal.in2p3.fr:2183:ztf:', 1000, 10000, null, false, null)
-//gr.processSourcesOfInterest('FINK_PORTAL', 'hbase-1.lal.in2p3.fr:2183:ztf:', 1000, 10000, new String[]{'SN candidate', 'Kilonova candidate', 'Early SN Ia candidate', 'Ambiguous', '*'},  false, null)
-//gr.processSourcesOfInterest('FINK_PORTAL', 'hbase-1.lal.in2p3.fr:2183:ztf:', 10000, 1500, new String[]{'*'},  false, null)
-gr.processSourcesOfInterest(new String[]{'FINK_PORTAL', 'FEATURES'}, 'hbase-1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 10000, 2500, null,  false, null)
+classes = new String[]{"(CTA) Blazar",
+                       "Ambiguous",
+                       "Early SN Ia candidate",
+                       "Kilonova candidate",
+                       "Microlensing candidate",
+                       "SN candidate",
+                       "Solar System candidate",
+                       "Solar System MPC",
+                       "Tracklet"}
+//gr.processSourcesOfInterest(new String[]{'FINK_PORTAL', 'FEATURES'}, 'hbase-1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 10000, 1500, null, false, null)
+gr.processSourcesOfInterest(new String[]{'FINK_PORTAL', 'FEATURES'}, 'hbase-1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 10000, 1500, classes, false, null)
 gr.generateCorrelations(Classifiers.valueOf("FEATURES"), Classifiers.valueOf("FINK_PORTAL"))
 gr.overlaps(null, null, "/tmp/overlaps.csv")
- 
