2c42d9ab6d86e808e32c4a4c588ff3ffd39d69df
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-04-29 14:17:15 +0200

    ...

--- src/work/LightCurves/plotLC.py
+++ src/work/LightCurves/plotLC.py
@@ -1,34 +1,66 @@
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-doPlot = True
-n = '0'
+doPlot = False
 dir = '../run/LightCurves/lstm_data'
+filename = '../data/LightCurves/2024/all.csv'
 set = 'train'
-
-jd  = np.loadtxt(dir + '/' + set + '/jds/jd_'       + n + '.csv', delimiter=',') 
-seq = np.loadtxt(dir + '/' + set + '/features/seq_' + n + '.csv', delimiter=',')
-
-with open(dir + '/' + set + '/oids/oid_' + n + '.csv', 'r') as f:
-  title = f.readline().strip()
+for n in range(0,10):
   
-with open(dir + '/' + set + '/labels/label_' + n + '.csv', 'r') as f:
-  title += ' (' + f.readline().strip() + ')'
+  jd  = np.loadtxt(dir + '/' + set + '/jds/jd_'       + str(n) + '.csv', delimiter=',') 
+  seq = np.loadtxt(dir + '/' + set + '/features/seq_' + str(n) + '.csv', delimiter=',')
+  
+  with open(dir + '/' + set + '/oids/oid_' + str(n) + '.csv', 'r') as f:
+    oid = f.readline().strip()
+    
+  with open(dir + '/' + set + '/labels/label_' + str(n) + '.csv', 'r') as f:
+    clazz = ':' + f.readline().strip()
 
-y1 = seq[:, 0]
-y2 = seq[:, 1]
+  print(oid)
 
-plt.figure(figsize=(10, 6))
-plt.plot(jd, y1, label='1', marker='o')
-plt.plot(jd, y2, label='2', marker='o')
-plt.xlabel('JD (transformed)')
-plt.ylabel('LC')
-plt.title(title)
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
+  with open(filename, 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+      if row['objectId'] == oid:
+        fids = np.array(row['collect_list(fid)'].split(';')).astype(int)
+        mags = np.array(row['collect_list(magpsf)'].split(';')).astype(float)
+        jds = np.array(row['collect_list(jd)'].split(';')).astype(float)       
+        mask_fid1 = fids == 1
+        mask_fid2 = fids == 2        
+        jd_fid1 = jds[mask_fid1]
+        mag_fid1 = mags[mask_fid1]        
+        jd_fid2 = jds[mask_fid2]
+        mag_fid2 = mags[mask_fid2]
+        break
+    
+  title = oid + ':' + clazz
+  
+  fig, axes = plt.subplots(2, 1, figsize=(10, 6))  
 
-plt.savefig('LC_' + n + '.png')
+  y1 = seq[:, 0]
+  y2 = seq[:, 1]
 
-if doPlot:
-  plt.show()
+  sorted_idx_fid1 = np.argsort(jd_fid1)
+  sorted_idx_fid2 = np.argsort(jd_fid2)
+  
+  axes[0].plot(jd, y1, label='1', color='orange', marker='o')
+  axes[0].plot(jd, y2, label='2', color='blue',   marker='o')
+  axes[0].set_xlabel('JD')
+  axes[0].set_ylabel('LC')
+  axes[0].set_title(title)
+  axes[0].legend()
+  axes[0].grid(True)
+  axes[1].plot(jd_fid1[sorted_idx_fid1], mag_fid1[sorted_idx_fid1], label='1', color='orange', marker='o')
+  axes[1].plot(jd_fid2[sorted_idx_fid2], mag_fid2[sorted_idx_fid2], label='2', color='blue',   marker='o')
+  axes[1].set_xlabel('JD')
+  axes[1].set_ylabel('LC')
+  axes[1].set_title(title)
+  axes[1].legend()
+  axes[1].grid(True)
+
+  plt.tight_layout()
+  plt.savefig(title + '.png')
+  
+  if doPlot:
+    plt.show()
