a71d92f4a08406da08fe98ea5ff85dcadbfd30b7
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-05-23 17:21:24 +0200

    ...

--- src/java/com/Lomikel/HBaser/HBaseClient.java
+++ src/java/com/Lomikel/HBaser/HBaseClient.java
@@ -794,7 +794,7 @@ public class HBaseClient extends Client<Table, HBaseSchema> {
     if (!key.startsWith("schema") &&_evaluator != null && !evaluateResult(r)) {
       return false;
       }
-    log.info(nnn);
+    log.info(nnn++);
     String[] ff;
     String ref;
     if (r.getRow() != null) {
