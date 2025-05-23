1e84f23a9ab9b3871186a3fe8017f94321559702
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-05-23 13:33:29 +0200

    ...

--- src/work/fillSOI.groovy
+++ src/work/fillSOI.groovy
@@ -5,18 +5,20 @@ import com.astrolabsoftware.FinkBrowser.Januser.Classifiers;
 jc = new JanusClient("/opt/janusgraph-1/conf/gremlin-server/IJCLab.properties")
 gr = new FinkGremlinRecipiesG(jc)
 
-classes = new String[]{"(CTA) Blazar",
-                       "Ambiguous",
-                       "Early SN Ia candidate",
-                       "Kilonova candidate",
-                       "Microlensing candidate",
-                       "SN candidate",
-                       "Solar System candidate",
-                       "Solar System MPC",
-                       "Tracklet",
-                       "Anomaly"}
 gr.fhclient('vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1').setEvaluation("cdsxmatch != 'Unknown' && roid != 3 && ndethist >= 3");
-gr.processSourcesOfInterest(new String[]{'FINK_PORTAL', 'FEATURES'}, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 100, 1500, null, false, null)
-//gr.processSourcesOfInterest(new String[]{'FINK_PORTAL', 'FEATURES'}, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 100, 1500, classes, false, null)
+gr.processSourcesOfInterest(new String[]{'FINK_PORTAL', 'FEATURES'}, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 1000, 1500, null, false, null)
+
+//classes = new String[]{"(CTA) Blazar",
+//                       "Ambiguous",
+//                       "Early SN Ia candidate",
+//                       "Kilonova candidate",
+//                       "Microlensing candidate",
+//                       "SN candidate",
+//                       "Solar System candidate",
+//                       "Solar System MPC",
+//                       "Tracklet",
+//                       "Anomaly"}
+//gr.processSourcesOfInterest(new String[]{'FINK_PORTAL', 'FEATURES'}, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 1000, 1500, classes, false, null)
+
 gr.generateCorrelations(Classifiers.valueOf("FEATURES"), Classifiers.valueOf("FINK_PORTAL"))
 gr.overlaps("outputCSV":"/tmp/overlaps.csv")
