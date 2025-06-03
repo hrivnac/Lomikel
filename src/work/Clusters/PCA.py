1456867d6ebbeb910ccb17bfb6dd200d7bc88b46
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-06-03 11:00:06 +0200

    ...

--- src/work/Clusters/PCA.py
+++ src/work/Clusters/PCA.py
@@ -92,6 +92,7 @@ args = ["cdsxmatch",
 
 df = df.withColumn("class", extract_fink_classification(*args))
 
+log.info(df.columns)
        
 df = df.filter(df.cdxmatch != "Unknown" & df.roid != 3 & df.ndethist >= 3)
 
