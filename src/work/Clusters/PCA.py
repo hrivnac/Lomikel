5b6666ffd4c4476068f98b227953d5a65ce9e408
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-06-03 10:54:19 +0200

    ...

--- src/work/Clusters/PCA.py
+++ src/work/Clusters/PCA.py
@@ -93,7 +93,7 @@ args = ["cdsxmatch",
 df = df.withColumn("class", extract_fink_classification(*args))
 
        
-df = df.filter(df.cdxmatch != "Unknown" && df.roid != 3 && df.ndethist >= 3)
+df = df.filter(df.cdxmatch != "Unknown" & df.roid != 3 & df.ndethist >= 3)
 
 # Converting lc_features arrays into columns -----------------------------------
       
