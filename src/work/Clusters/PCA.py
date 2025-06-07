d941630391159b471fed53a85d9a44662130e53c
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-06-07 18:00:07 +0200

    ...

--- src/work/Clusters/PCA.py
+++ src/work/Clusters/PCA.py
@@ -53,9 +53,9 @@ n_clusters_start = 5
 n_clusters = 50
 silhouette = False
 cluster_features = "pca_features"
-known = False
+known = True
 tag = ""
-clean = True
+clean = False
 
 # Clean ------------------------------------------------------------------------
 
