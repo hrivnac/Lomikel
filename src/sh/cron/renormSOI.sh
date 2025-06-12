f68bc20a38d76d3a4be7db49c6c633f6c018c5e6
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-06-12 17:19:16 +0200

    ...

---
+++ src/sh/cron/renormSOI.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash -l                                                                                                                                                                                                                                                
+NOW=`date +"%Y%m%d%H%M%s"`
+LOG=/tmp/renorm-${NOW}.log
+LOCK=/tmp/renorm.lock
+if [[ -e ${LOCK} ]]; then
+  echo "Already renorming ${LOCK}"
+  exit
+  fi
+PID=$$
+echo ${PID} > ${LOCK}
+cd ~/Lomikel/ant
+source ./setup.sh
+java -jar ~/Lomikel/lib/Lomikel-${version}-Janus.exe.jar -b -s ~/Lomikel/src/work/renormSOI.groovy | tee -a ${LOG} 2>&1
+/bin/rm -f ${LOCK}
+
