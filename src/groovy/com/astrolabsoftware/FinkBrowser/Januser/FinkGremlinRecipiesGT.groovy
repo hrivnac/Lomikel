6f14c1fd1bda84177c62faf301eb32dece1ad0d4
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-06-17 11:33:27 +0200

    ...

--- src/groovy/com/astrolabsoftware/FinkBrowser/Januser/FinkGremlinRecipiesGT.groovy
+++ src/groovy/com/astrolabsoftware/FinkBrowser/Januser/FinkGremlinRecipiesGT.groovy
@@ -37,6 +37,8 @@ import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.inV;
 import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.constant;
 import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.identity;
 import static org.apache.tinkerpop.gremlin.process.traversal.P.within;
+import static org.apache.tinkerpop.gremlin.process.traversal.P.neq;
+import static org.apache.tinkerpop.gremlin.process.traversal.P.eq;
 import static org.apache.tinkerpop.gremlin.process.traversal.Order.asc;
 
 // Janus Graph
