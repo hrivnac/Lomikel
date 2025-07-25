5cb4354e3d47a78efd61b5f0d009f2e21d90f7f2
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-07-25 14:38:14 +0200

    ...

--- src/work/fillSOI.groovy
+++ src/work/fillSOI.groovy
@@ -9,7 +9,7 @@ gr = new FinkGremlinRecipiesG(jc)
 FeaturesClassifier.setModelResource("Clusters/2024/13-60")
 
 formula = "cdsxmatch != 'Unknown' && roid != 3 && ndethist >= 3";
-gr.processSoI(new String[]{'FINK_PORTAL', 'FEATURES'}, formula, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 20000, 1500, null, false, null)
+gr.processSoI(new String[]{'FINK', 'FEATURES'}, formula, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 20000, 1500, null, false, null)
 
 //classes = new String[]{"(CTA) Blazar",
 //                       "Ambiguous",
@@ -21,7 +21,7 @@ gr.processSoI(new String[]{'FINK_PORTAL', 'FEATURES'}, formula, 'vdhbase1.lal.in
 //                       "Solar System MPC",
 //                       "Tracklet",
 //                       "Anomaly"}
-//gr.processSoI(new String[]{'FINK_PORTAL', 'FEATURES'}, null, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 0, 1500, classes, false, null)
+//gr.processSoI(new String[]{'FINK', 'FEATURES'}, null, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 0, 1500, classes, false, null)
 
 gr.generateCorrelations(Classifiers.valueOf("FEATURES"), Classifiers.valueOf("FINK_PORTAL"))
 gr.overlaps("outputCSV":"/tmp/overlaps.csv")
