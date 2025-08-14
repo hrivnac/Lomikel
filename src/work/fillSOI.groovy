2e31cf5eb6f18f3fe093a88cff7c030141abf3f9
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-08-14 12:50:28 +0200

    ...

--- src/work/fillSOI.groovy
+++ src/work/fillSOI.groovy
@@ -10,9 +10,11 @@ classifiers = new Classifier[]{Classifier.instance('FINK'),
                                Classifier.instance('FEATURES=Clusters/2024/13-60'),
                                Classifier.instance('FEATURES=Clusters/2025/13-50')}
 formula = "cdsxmatch != 'Unknown' && roid != 3 && ndethist >= 3";
-gr.processSoI(classifiers, formula, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 20000, 1500, null)
+//gr.processSoI(classifiers, formula, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 20000, 1500, null)
 
-gr.overlaps("outputCSV":"/tmp/overlaps.csv")
+gr.processSoI(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Microlensing candidate"})
+gr.processSoI(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Solar System candidate"})
+gr.processSoI(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Solar System MPC"})
 
 
 
@@ -25,15 +27,3 @@ gr.overlaps("outputCSV":"/tmp/overlaps.csv")
 //==>Solar System candidate=12
 //==>Solar System MPC=7
 
-import com.Lomikel.Januser.JanusClient;
-import com.astrolabsoftware.FinkBrowser.Januser.FinkGremlinRecipiesG;
-import com.astrolabsoftware.FinkBrowser.Januser.Classifier;
-
-classifiers = new Classifier[]{Classifier.instance('FINK'),
-                               Classifier.instance('XMATCH'),
-                               Classifier.instance('FEATURES=Clusters/2024/13-60'),
-                               Classifier.instance('FEATURES=Clusters/2025/13-50')}
-
-clss = new String[]{"Early SN Ia candidate"}
-
-gr.processSoI(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 200, 1500, clss)
