70d9a718be6611945d8edfc63f8c99401d8b8bf6
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-08-14 12:54:25 +0200

    ...

--- src/work/fillSOI.groovy
+++ src/work/fillSOI.groovy
@@ -10,11 +10,11 @@ classifiers = new Classifier[]{Classifier.instance('FINK'),
                                Classifier.instance('FEATURES=Clusters/2024/13-60'),
                                Classifier.instance('FEATURES=Clusters/2025/13-50')}
 formula = "cdsxmatch != 'Unknown' && roid != 3 && ndethist >= 3";
-//gr.processSoI(classifiers, formula, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 20000, 1500, null)
+gr.processSoI(classifiers, formula, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 20000, 1500, null)
 
-gr.processSoI(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Microlensing candidate"})
-gr.processSoI(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Solar System candidate"})
-gr.processSoI(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Solar System MPC"})
+//gr.processSoI(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Microlensing candidate"})
+//gr.processSoI(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Solar System candidate"})
+//gr.processSoI(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Solar System MPC"})
 
 
 
