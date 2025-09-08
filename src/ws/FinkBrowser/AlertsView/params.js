85178072c87e9e2ce2882af5e5a7413432dc7499
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-09-08 21:03:40 +0200

    ...

---
+++ src/ws/FinkBrowser/AlertsView/params.js
@@ -0,0 +1,13 @@
+// Color mapping by alert class
+const classes = {
+  "Microlensing candidate": "255,255,0",
+  "Early SN Ia candidate": "0,255,255",
+  "SN candidate": "255,0,0",
+  "Solar System candidate": "0,255,0",
+  "Solar System MPC": "255,0,255"
+  };
+
+let fetchPeriod = 10; // every x minutes
+let fetchStart = 24; // hours before now
+let nAlerts = 10;
+  
---
+++ src/ws/FinkBrowser/LightCurveView/params.js
@@ -0,0 +1,12 @@
+const bandColors = {
+  "Y": "black",
+  "z": "red",
+  "g": "blue",
+  "i": "grey",
+  "u": "purple",
+  "r": "green"
+  };
+
+const snid_selection = [99141878, 98988375, 39702066, 98837255];
+
+const demo_selection = ["random", "peak", "periodic"];
