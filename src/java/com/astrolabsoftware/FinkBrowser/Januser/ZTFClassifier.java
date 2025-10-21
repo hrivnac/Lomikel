841159f0a3d16c8c03f2eefa5d7c3d94b2092c66
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-10-21 13:57:55 +0200

    ...

---
+++ src/java/com/astrolabsoftware/FinkBrowser/Januser/ZTFClassifier.java
@@ -0,0 +1,18 @@
+package com.astrolabsoftware.FinkBrowser.Januser;
+
+import com.Lomikel.Utils.Pair;
+import com.Lomikel.Utils.LomikelException;
+
+// Java
+import java.util.Map;
+import java.util.HashMap;
+
+/** <code>ZTFClassifier</code> classifies <em>ZTF</em> <em>objects</em>.
+  * @opt attributes
+  * @opt operations
+  * @opt types
+  * @opt visibility
+  * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
+public abstract class ZTFClassifier extends Classifier {
+   
+  }
