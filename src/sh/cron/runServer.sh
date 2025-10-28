a044052e8edd0e1cc0a839b9bda4cb4aac677122
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-10-28 15:26:04 +0100

    ...

---
+++ src/sh/cron/runServer.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash -l
+NOW=`date +"%Y%m%d%H%M%s"`
+LOG=/tmp/runServer-${NOW}.log
+LOCK=/tmp/runServer.lock
+if [[ -e ${LOCK} ]]; then
+  echo "Running Server with ${LOCK}"
+  kill `cat ${LOCK}`
+  kill -9 `cat ${LOCK}`
+  fi
+if [[ -e ${LOCK} ]]; then
+  echo "  not killed ${LOCK}"
+  exit
+  fi
+echo "  killed"
+/bin/rm -f ${PID}
+PID=$$
+echo ${PID} > ${LOCK}
+cd ~/Lomikel/ant
+source ./setup.sh
+java -jar /localhome/janusgraph/Lomikel/extlib/jetty-runner-9.4.54.v20240208.jar --path /FinkBrowser --port 8888 --stop-port 8889 --stop-key stop --log FinkBrowserServer.log --out FinkBrowserServer.out ../lib/FinkBrowser.war
+/bin/rm -f ${LOCK}
