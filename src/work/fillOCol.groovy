3273d2885ddc36fc779f9ce268f55a14f49f9b39
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-10-28 14:49:34 +0100

    ...

--- src/work/fillOCol.groovy
+++ src/work/fillOCol.groovy
@@ -8,10 +8,9 @@ gr = new FinkGremlinRecipiesG(jc)
 classifiers = new Classifier[]{Classifier.instance('FINK'),
                                Classifier.instance('XMATCH'),
                                Classifier.instance('FEATURES=2024/13-60'),
-                               Classifier.instance('FEATURES=2025/13-50'),
-                               Classifier.instance('TAG')}
-//formula = "cdsxmatch != 'Unknown' && roid != 3 && ndethist >= 3";
-gr.processOCol(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 20000, 1500, null);
+                               Classifier.instance('FEATURES=2025/13-50')}
+formula = "cdsxmatch != 'Unknown' && roid != 3 && ndethist >= 3";
+gr.processOCol(classifiers, formula, 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 20000, 1500, null);
 
 //gr.processOCol(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Microlensing candidate"})
 //gr.processOCol(classifiers, 'true', 'vdhbase1.lal.in2p3.fr:2183:ztf:schema_4.0_6.1.1', 2000000, 1500000, new String[]{"Solar System candidate"})
