e15b328449b6afe2e292960db7e86e8f7904a8db
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-10-29 10:50:25 +0100

    ...

--- src/ws/FinkBrowser/ClassificationView/drawing.js
+++ src/ws/FinkBrowser/ClassificationView/drawing.js
@@ -48,7 +48,7 @@ async function showObjectNeighborhood(data) {
                       .x(d => d.x)
                       .y(d => d.y)
                       .curve(d3.curveLinearClosed);
-  console.log(classList.map(cls => classPositions[cls]));
+  console.log(classList.map(cls => classPositions[cls]).sort((a, b) => a.angle - b.angle));
   container.append("path")
            .datum(classList.map(cls => classPositions[cls]))
            .attr("class", "link-line")
