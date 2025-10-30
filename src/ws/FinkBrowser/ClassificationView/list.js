76c63fb61dc5d826df7a092fa36f10f5a10362e1
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-10-30 14:40:05 +0100

    ...

--- src/ws/FinkBrowser/ClassificationView/list.js
+++ src/ws/FinkBrowser/ClassificationView/list.js
@@ -5,49 +5,23 @@ function updateDetailsPanel(data) {
   const rows = [];
 
   rows.push(`<div class="objLine mainObj">
-               <div>${data.objectId}</div>
-               <div>${JSON.stringify(data.objectClassification)}</div>
+               <div><b>${data.objectId}</b></div>
+               <div>${JSON.stringify(data.objectClassification).replaceAll("{", "").replaceAll("}", "").replaceAll(",", "<br/>"}</div>
                </div>`);
   
   for (var [key, val] of iterate_object(data.objects)) {
     rows.push(`<div class="objLine">
-                 <div>${key} ${val.distance}</div>
-                 <div>${JSON.stringify(val.classes)}</div>
+                 <div><b>${key}</b> ${val.distance}</div>
+                 <div>${JSON.stringify(val.classes).replaceAll("{", "").replaceAll("}", "").replaceAll(",", "<br/>"}</div>
                  </div>`);
     }
-
-/*
-
-  // Add all other visible objects
-  for (const obj of objects) {
-    if (obj.id === mainObjectId) continue;
-    const dx = obj.x - mainPos.x;
-    const dy = obj.y - mainPos.y;
-    const distance = Math.sqrt(dx*dx + dy*dy).toFixed(2);
-    rows.push(`
-      <div class="objLine">
-        <div>${obj.name || obj.id}</div>
-        <div>Distance: ${distance}</div>
-        <div>Classes: ${formatClasses(obj.classes)}</div>
-      </div>
-    `);
-  }
-*/
+    
   panel.innerHTML = rows.join("");
-  
-}
+  }
 
 function* iterate_object(o) {
     var keys = Object.keys(o);
     for (var i=0; i<keys.length; i++) {
         yield [keys[i], o[keys[i]]];
     }
-}
-
-function formatClasses(classData) {
-  if (!classData) return "—";
-  return Object.entries(classData)
-               .map(([cls, weight]) => `${cls} (${weight.toFixed(2)})`)
-               .join(", ");
   }
-
