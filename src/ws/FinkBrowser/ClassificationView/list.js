5dbf379d95dd9d458ae455a2c21e3b9d30d3e656
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-10-31 14:14:17 +0100

    ...

--- src/ws/FinkBrowser/ClassificationView/list.js
+++ src/ws/FinkBrowser/ClassificationView/list.js
@@ -11,16 +11,23 @@ function updateDetailsPanel(data) {
   row += `</div></div>`;
   rows.push(row);
     
-  console.log(objects(data.objects)); 
-  
-  for (var [key, val] of iterate_object(data.objects)) {
-    row = `<div class="objLine"><div><b><u>${key}</b>: ${val.distance.toFixed(4)}</u> (<a href="https://fink-portal.org/${key}" target="_blank">Fink</a>)</div><div>`;
-    for (var [k, v] of iterate_object(val.classes)) {
+  for (var o of objects(data.objects)) {
+    row = `<div class="objLine"><div><b><u>${o.objectId}</b>: ${o.v.distance.toFixed(4)}</u> (<a href="https://fink-portal.org/${key}" target="_blank">Fink</a>)</div><div>`;
+    for (var [k, v] of iterate_object(o.v.classes)) {
       row += `${k}: ${v.toFixed(4)}<br/>`;
       }
     row += `</div></div>`;
     rows.push(row);
     }
+
+  //for (var [key, val] of iterate_object(data.objects)) {
+  //  row = `<div class="objLine"><div><b><u>${key}</b>: ${val.distance.toFixed(4)}</u> (<a href="https://fink-portal.org/${key}" target="_blank">Fink</a>)</div><div>`;
+  //  for (var [k, v] of iterate_object(val.classes)) {
+  //    row += `${k}: ${v.toFixed(4)}<br/>`;
+  //    }
+  //  row += `</div></div>`;
+  //  rows.push(row);
+  //  }
 
   panel.innerHTML = rows.join("");
   }
@@ -38,7 +45,6 @@ function objects(o) {
   for (var i = 0; i  < keys.length; i++) {
     os.push({objectId:keys[i], v:o[keys[i]]});
     }
-  console.log(os);
   let oss = os.sort((a, b) => a.v.distance - b.v.distance);
   return oss;
   }
