ad154cf883237b4cc95f6d9ec9342a8fdf6ac0a1
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-01-27 13:16:03 +0100

    ...

--- src/ws/LoadPane.js
+++ src/ws/LoadPane.js
@@ -11,7 +11,12 @@ async function loadPane(pane, url, iframe, height) {
     }
   if (iframe) {
     console.log("iframe");
-    document.getElementById(pane).innerHTML='<iframe height="' + height + '" width="100%" src="' + url + '">';
+    //document.getElementById(pane).innerHTML='<iframe height="' + height + '" width="100%" src="' + url + '">';
+    document.getElementById(pane).innerHTML='<iframe name="dynamicFrame" height="' + height + '" width="100%">';
+    postUrlToIframe(
+    url,
+    "dynamicFrame"
+      );
     }
   else {
     console.log("load");
@@ -22,3 +27,31 @@ async function loadPane(pane, url, iframe, height) {
     w2ui['layoutMain']['panels'][0]['tabs'].click(pane.concat("Tab"));
     }
   }        
+
+function postUrlToIframe(fullUrl, iframeName) {
+    // Parse URL
+    const url = new URL(fullUrl, window.location.origin);
+    const action = url.origin + url.pathname;
+
+    // Create form
+    const form = document.createElement("form");
+    form.method = "POST";
+    form.action = action;
+    form.target = iframeName;
+    form.style.display = "none";
+
+    // Convert query params to hidden inputs
+    url.searchParams.forEach((value, key) => {
+        const input = document.createElement("input");
+        input.type = "hidden";
+        input.name = key;
+        input.value = value;
+        form.appendChild(input);
+    });
+
+    // Add form to DOM, submit, then clean up
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removeChild(form);
+}
+
