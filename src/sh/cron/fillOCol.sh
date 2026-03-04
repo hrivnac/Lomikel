ff64e028c0b8044c7f6013b9c8cf53cdc22ff5f1
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-03-04 18:31:08 +0100

    ...

--- src/sh/cron/fillOCol.sh
+++ src/sh/cron/fillOCol.sh
@@ -3,12 +3,12 @@ NOW=`date +"%Y%m%d%H%M%s"`
 LOG=/tmp/fillOCol-${NOW}.log
 LOCK=/tmp/fillOCol.lock 
 if [[ -e ${LOCK} ]]; then
-  echo "Already filling SoI with ${LOCK}"
+  echo "Already filling OCol with ${LOCK}"
   exit
   fi
 PID=$$
 echo ${PID} > ${LOCK}
 cd ~/Lomikel/ant
 source ./setup.sh
-java -jar ~/Lomikel/lib/Lomikel-Janus-${version}.exe.jar -b -s ~/Lomikel/src/work/fillOCol.groovy | tee -a ${LOG} 2>&1
+java -jar ~/Lomikel/lib/Lomikel-Janus-${version}.exe.jar -b -s ~/Lomikel/src/work/fillOColNew.groovy | tee -a ${LOG} 2>&1
 /bin/rm -f ${LOCK}
