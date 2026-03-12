d888389f690e8de01ef9b3977ce1e04172e4bacd
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-03-12 14:36:39 +0100

    ...

--- ant/setup.sh
+++ ant/setup.sh
@@ -43,7 +43,9 @@ alias gremlin_CC='CLASSPATH="" ${janusgraph_dir}/bin/gremlin.sh -i "../src/greml
 alias lomikel='CLASSPATH="${LOMIKEL_CLASSPATH}" java com.Lomikel.Apps.LUC'
 alias lomikel_pure='java -jar ../lib/Lomikel-${version}.exe.jar'
 alias lomikel_hbase='java -jar ../lib/Lomikel-HBase-${version}.exe.jar'
+alias lomikel_hadoop='java -jar ../lib/Lomikel-Hadoop-${version}.exe.jar'
 alias lomikel_janus='java -jar ../lib/Lomikel-Janus-${version}.exe.jar'
 alias lomikel_dl4j='java --enable-preview -jar ../lib/Lomikel-dl4j-${version}.exe.jar'
 
-echo "commands: gremlin_console_Local, gremlin_console_IJCLab, gremlin_console_CC, gremlin_Local, gremlin_IJCLab, gremlin_CC, lomikel, lomikel_pure, lomikel_hbase, lomikel_janus, lomikel_dl4j"
+echo "commands: gremlin_console_Local, gremlin_console_IJCLab, gremlin_console_CC, gremlin_Local, gremlin_IJCLab, gremlin_CC, lomikel, lomikel_pure, lomikel_hbase, lomikel_hadoop, lomikel_janus, lomikel_dl4j"
+ 
