d3410ea2bb97c28c08f6e3b92f6e235f4e7fe91e
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-03-13 11:37:03 +0100

    ...

--- src/java/com/Lomikel/Parquet/ParquetReader.java
+++ src/java/com/Lomikel/Parquet/ParquetReader.java
@@ -176,7 +176,7 @@ public class ParquetReader {
                               int       j,
                               String    gname) {
     String fullname = name;
-    if (gname != null) {
+    if (gname != null && !name.equals("list")) {
       fullname = gname + "." + name;
       }
     switch (type) {
