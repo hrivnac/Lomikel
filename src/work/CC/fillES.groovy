5f3ab86a17abc03b68b2e40d3ccf565cac6fffbe
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-03-13 15:07:36 +0100

    ...

--- src/work/CC/fillES.groovy
+++ src/work/CC/fillES.groovy
@@ -14,10 +14,11 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.config.Configurator;
 
+Configurator.initialize(null, "../src/java/log4j2.xml");
+
 public class PR extends ParquetReader {
 
   ESClient esclient = new ESClient("http://157.136.253.253:20200");
-  Configurator.initialize(null, "../src/java/log4j2.xml");
   log = LogManager.getLogger(this.class);
 
   public PR(String url) {
