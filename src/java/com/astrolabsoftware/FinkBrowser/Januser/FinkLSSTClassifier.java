42498d2609697114627531ba88b0af0089469a3e
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-04-02 13:57:45 +0200

    ...

--- src/java/com/astrolabsoftware/FinkBrowser/Januser/FinkLSSTClassifier.java
+++ src/java/com/astrolabsoftware/FinkBrowser/Januser/FinkLSSTClassifier.java
@@ -103,22 +103,26 @@ public class FinkLSSTClassifier extends LSSTClassifier {
     }
   
   private static Map<String, HBaseClient> CLIENTS;
-  
+
+  private static Set<String> CLASSES = Set.of("rubin.tag_early_snia_candidate");
+   
   /** Logging . */
   private static Logger log = LogManager.getLogger(FinkLSSTClassifier.class);
   
   static {
     CLIENTS = new TreeMap<String, HBaseClient>();
     HBaseClient client;
-    try {
-      client = new HBaseClient("cchbase1.in2p3.fr", 2183);
-      client.connect("rubin.tag_early_snia_candidate", "schema_4.1_8.39.0");
-      CLIENTS.put("rubin.tag_early_snia_candidate", client);
+    for (String cls : CLASSES) {
+      try {
+        client = new HBaseClient("cchbase1.in2p3.fr", 2183);
+        client.connect(cls, "schema_4.1_8.39.0");
+        CLIENTS.put(cls, client);
+        }
+      catch (LomikelException e) {
+        log.error("Cannot connect to rubin.tag_early_snia_candidate table");
+        }
       }
-    catch (LomikelException e) {
-      log.error("Cannot connect to rubin.tag_early_snia_candidate table");
-      }
-    }
+    }  
  
   }
            
