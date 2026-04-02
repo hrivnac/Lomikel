c2a53268787904539c962509015ef2f65b2cf8db
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-04-02 10:46:42 +0200

    ...

--- src/work/CC/fillOCol.groovy
+++ src/work/CC/fillOCol.groovy
@@ -1,4 +1,8 @@
 import com.Lomikel.Parquet.ParquetReader;
+import com.Lomikel.Januser.JanusClient;
+import com.astrolabsoftware.FinkBrowser.Januser.FinkGremlinRecipiesG;
+import com.astrolabsoftware.FinkBrowser.Januser.Classifier;
+import com.Lomikel.Utils.Timer;
 
 // Parquet
 import org.apache.parquet.schema.GroupType;
@@ -18,12 +22,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.config.Configurator;
 
 Configurator.initialize(null, "../src/java/log4j2.xml");
+Logger log = LogManager.getLogger(this.class)
 
 delay = 25;
 
 public class PR extends ParquetReader {
 
-  Logger log = LogManager.getLogger(this.class);
+  //Logger log = LogManager.getLogger(this.class);
 
   public PR(String url) {
     super(url);
@@ -32,11 +37,8 @@ public class PR extends ParquetReader {
   @Override
   protected void addToSet(String name,
                           String value) {
-    if (name.equals("diaSource.ra"            ) ||
-        name.equals("diaSource.dec"           ) ||
-        name.equals("diaSource.midpointMjdTai") ||
-        name.equals("diaObject.diaObjectId"   ) ||
-        name.equals("ssSource.ssObjectId"     )) {
+    if (name.equals("diaObject.diaObjectId") ||
+        name.equals("ssSource.ssObjectId"  )) {
       Set<String> set;
       if (props().containsKey(name)) {
         set = props().get(name);
@@ -52,30 +54,11 @@ public class PR extends ParquetReader {
   @Override
   public void endGroup() {
     log.info(props());
-    //if (props().containsKey("diaObject.diaObjectId") || props().containsKey("ssSource.ssObjectId")) {
-    //  double ra  = new Double(props().get("diaSource.ra"            ).iterator().next());
-    //  double dec = new Double(props().get("diaSource.dec"           ).iterator().next());
-    //  double mjd = new Double(props().get("diaSource.midpointMjdTai").iterator().next());
-    //  String key;
-    //  if (props().containsKey("diaObject.diaObjectId")) {
-    //    key = props().get("diaObject.diaObjectId").iterator().next();
-    //    esclient.putGeoPoint("dia_radec", "location", key, ra, dec);
-    //    esclient.putValue("dia_mjd", "mjd", key, mjd);
-    //    }
-    //  else if (props().containsKey("ssSource.ssObjectId")) {
-    //    key = props().get("ssSource.ssObjectId");
-    //    esclient.putGeoPoint("ss_radec", "location", key, ra, dec);
-    //    esclient.putValue("ss_mjd", "mjd", key, mjd );
-    //    }
-    //  else {
-    //    log.warn("no objectid");
-    //    }
-    //  props().clear();
-    //  esclient.commit();
-    //  }
     }
     
   }
+
+timer = new Timer("entries", 100, 5); 
   
 reader = new PR("hdfs://ccmaster1:8020");
 yesterday = LocalDate.now().minusDays(delay).format(DateTimeFormatter.ofPattern("'year='yyyy'/month='MM'/day='dd"));
