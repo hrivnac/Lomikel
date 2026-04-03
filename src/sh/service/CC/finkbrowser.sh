660e3a3f276d4d2ce491b119671a44a288dcae85
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-04-03 13:24:45 +0200

    ...

---
+++ src/sh/service/CC/finkbrowser.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+exec /usr/lib/jvm/java-17-openjdk-17.0.18.0.8-1.el9.alma.1.x86_64/bin/java \
+-jar '/home/almalinux/Lomikel/extlib/jetty-runner-9.4.54.v20240208.jar' \
+--path '/FinkBrowser' \
+--port 8888 \
+/home/almalinux/Lomikel/lib/FinkBrowser.war
+
+/usr/lib/jvm/java-17-openjdk-17.0.18.0.8-1.el9.alma.1.x86_64/bin/java
