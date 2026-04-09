69d81182c22d499f291a0c667d766722a5408f33
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-04-09 11:33:49 +0200

    ...

---
+++ src/work/CC/importTags.groovy
@@ -0,0 +1,60 @@
+import com.Lomikel.HBaser.AsynchHBaseClient;
+import com.Lomikel.Januser.JanusClient;
+import com.astrolabsoftware.FinkBrowser.Januser.FinkGremlinRecipiesG;
+import com.astrolabsoftware.FinkBrowser.Januser.Classifier;
+import com.Lomikel.Utils.Timer;
+
+// Log
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.config.Configurator;
+
+Configurator.initialize(null, '../src/java/log4j2.xml');
+log = LogManager.getLogger(this.class);
+
+timer = new Timer("entries", 100, 5);
+
+client = new AsynchHBaseClient("cchbase1.in2p3.fr", 2183);
+client.setMaxQueueSize(100);
+
+clss = new String[]{'rubin.tag_early_snia_candidate',
+                    'rubin.tag_extragalactic_lt20mag_candidate',
+                    'rubin.tag_extragalactic_new_candidate',
+                    'rubin.tag_good_quality',
+                    'rubin.tag_hostless_candidate',
+                    'rubin.tag_in_tns',
+                    'rubin.tag_sn_near_galaxy_candidate'};
+
+jc = new JanusClient("/opt/janusgraph-1/conf/gremlin-server/CC.properties");
+gr = new FinkGremlinRecipiesG(jc);
+
+now = System.currentTimeMillis();
+
+timer.start();
+
+clss.each {
+  cls -> log.info('importing ' + cls);
+         client.connect(cls, '');
+         client.startScan(null,
+                          null,
+                          null,
+                          0,
+                          now,
+                          false,
+                          false);
+  while (client.scanning() || client.size() > 0) {
+    if (client.size() > 0) {
+      client.poll().each {k, v -> (oid, mjd) = k.tokenize('_');
+                                   gr.g().addV('NewTag') \
+                                     .property('lbl', 'NewTag') \
+                                     .property('objectId', oid) \
+                                     .property('cls', cls) \
+                                     .property('mjd', mjd)
+                                     .iterate();
+                           }
+      timer.report();
+      }
+    }
+  client.stop();
+  }
+
