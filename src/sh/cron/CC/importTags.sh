9e0650564aebf6069ec74f743055e5ca18c29b63
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-04-10 14:12:18 +0200

    ...

--- src/sh/cron/CC/importTags.sh
+++ src/sh/cron/CC/importTags.sh
@@ -17,6 +17,6 @@ for T in rubin.tag_early_snia_candidate \
          rubin.tag_hostless_candidate \
          rubin.tag_in_tns \
          rubin.tag_sn_near_galaxy_candidate; do
-  java -jar ~/Lomikel/lib/Lomikel-Janus-${version}.exe.jar -b -s ~/Lomikel/src/work/CC/importTags.groovy -o "cls='rubin.tag_early_snia_candidate'" | tee -a ${LOG} 2>&1
+  java -jar ~/Lomikel/lib/Lomikel-Janus-${version}.exe.jar -b -s ~/Lomikel/src/work/CC/importTags.groovy -o "cls='${T}'" | tee -a ${LOG} 2>&1
   done
 /bin/rm -f ${LOCK} 
