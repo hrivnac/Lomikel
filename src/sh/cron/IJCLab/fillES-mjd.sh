7106545b95b714b95b7d9a4233570fde4475337b
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-05-07 21:57:53 +0200

    ...

---
+++ src/sh/cron/IJCLab/fillES-mjd.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash -l
+NOW=`date +"%Y%m%d%H%M%s"`
+LOG=/tmp/fillES-mjd-${NOW}.log
+LOCK=/tmp/fillES-mjd.lock 
+if [[ -e ${LOCK} ]]; then
+  echo "Already filling ES-mjd with ${LOCK}"
+  exit
+  fi
+PID=$$
+echo ${PID} > ${LOCK}
+cd ~/Lomikel/ant
+source ./setup.sh
+java --add-opens=java.base/java.lang=ALL-UNNAMED -jar ~/Lomikel/lib/Lomikel-All-${version}.exe.jar -b -s ~/Lomikel/src/work/IJCLab/fillES-mjd.groovy | tee -a ${LOG} 2>&1
+/bin/rm -f ${LOCK} 
+
