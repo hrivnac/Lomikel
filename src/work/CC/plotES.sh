ef886c97d4d2095a40302259a6ed8cc23db8f7a7
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-06-10 12:46:57 +0200

    ...

--- src/work/CC/plotES.sh
+++ src/work/CC/plotES.sh
@@ -1,5 +1,6 @@
-python3 ../src/work/CC/plotES-dia-radec.py    --max-points 100000 --output s_dia_radec.png
-python3 ../src/work/CC/plotES-ss-radec-top.py                     --output s_ss_radec.png
+python3 ../src/work/CC/plotES-dia-radec.py      --max-points 100000             --output s_dia_radec.png
+python3 ../src/work/CC/plotES-dia-radec-last.py --number 100000                 --output s_dia_radec_last.png
+python3 ../src/work/CC/plotES-ss-radec-top.py                                   --output s_ss_radec_top.png
 python3 ../src/work/CC/plotES-hist.py --index ss_radec --field location --log-y --output h_ss_radec.png
 python3 ../src/work/CC/plotES-hist.py --index ss_mjd   --field mjd      --log-y --output h_ss_mjd.png
 python3 ../src/work/CC/plotES-hist.py --index dia_mjd  --field mjd      --log-y --output h_dia_mjd.png
