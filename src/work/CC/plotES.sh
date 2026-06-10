84957f4b6e195da88cb5245ae3cbbc7b88412552
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-06-10 14:51:50 +0200

    ...

--- src/work/CC/plotES.sh
+++ src/work/CC/plotES.sh
@@ -1,9 +1,9 @@
-python3 ../src/work/CC/plotES-dia-radec.py             --max-points 100000              --output s_dia_radec.png
-python3 ../src/work/CC/plotES-dia-radec-latest.py      --number 100000                  --output s_dia_radec_latest.png
-python3 ../src/work/CC/plotES-ss-radec-top.py                                           --output s_ss_radec_top.png
-python3 ../src/work/CC/plotES-ss-radec-latest.py       --number 10      --min-points 10 --output s_ss_radec_latest.png
-python3 ../src/work/CC/plotES-hist.py --index ss_radec --field location --log-y         --output h_ss_radec.png
-python3 ../src/work/CC/plotES-hist.py --index ss_mjd   --field mjd      --log-y         --output h_ss_mjd.png
-python3 ../src/work/CC/plotES-hist.py --index radec    --field location --log-y         --output h_dia_radec.png
-python3 ../src/work/CC/plotES-hist.py --index dia_mjd  --field mjd      --log-y         --output h_dia_mjd.png
+python3 ../src/work/CC/plotES-dia-radec.py              --max-points 100000              --output s_dia_radec.png
+python3 ../src/work/CC/plotES-dia-radec-latest.py       --number 100000                  --output s_dia_radec_latest.png
+python3 ../src/work/CC/plotES-ss-radec-top.py                                            --output s_ss_radec_top.png
+python3 ../src/work/CC/plotES-ss-radec-latest.py        --number 10      --min-points 10 --output s_ss_radec_latest.png
+python3 ../src/work/CC/plotES-hist.py --index ss_radec  --field location --log-y         --output h_ss_radec.png
+python3 ../src/work/CC/plotES-hist.py --index ss_mjd    --field mjd      --log-y         --output h_ss_mjd.png
+python3 ../src/work/CC/plotES-hist.py --index dia_radec --field location --log-y         --output h_dia_radec.png
+python3 ../src/work/CC/plotES-hist.py --index dia_mjd   --field mjd      --log-y         --output h_dia_mjd.png
 echo "scp almalinux@134.158.243.139:/home/almalinux/Lomikel/ant/'*'.png ./"
