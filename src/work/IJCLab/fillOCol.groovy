16eb76b5c4bf0f4ece16290896818e1a5aa2472d
Julius Hrivnac <julius.hrivnac@cern.ch>
2026-08-12 11:50:54 +0200

    ...

--- src/work/IJCLab/fillOCol.groovy
+++ src/work/IJCLab/fillOCol.groovy
@@ -71,4 +71,4 @@ gr.generateCorrelations(classifiers);
 //client.stop();
 
 // BUG: why doesn't work in thread ?
-NotifierURL.notifyExecution("importTags-ZTF", "Lomikel", Info.release(), timer.info(cls + "[" + delay + "]: "));
+NotifierURL.notifyExecution("importTags-ZTF", "Lomikel", Info.release(), timer.info("" + delay);
